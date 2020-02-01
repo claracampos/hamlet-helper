e3425d299f3837afa8a12ad5c6c8ff5f150ff511
claracampos <camposclr@gmail.com>
2020-02-01 16:54:34 -0400

    implemented lettering plugin

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import logDefinition from "./utils/logDefinition";
 function App() {
   return (
     <div className="App">
-      <p onClick={logDefinition}>
-        <span>Long</span> <span>live</span> <span>the</span> <span>king</span>!
+      <p className="word-split" onClick={logDefinition}>
+        This is an example. You are testing.
       </p>
     </div>
   );
