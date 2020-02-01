a59bbb0596ae709e636eaf67acbabf3657f3fb0e
claracampos <camposclr@gmail.com>
2020-02-01 16:53:28 -0400

    worked around possible errors

--- src/utils/getDefinition.js
+++ src/utils/getDefinition.js
@@ -1,15 +1,17 @@
 const getDefinition = response => {
-  const headword = response.data[0].hwi.hw;
+  if (response.data[0]) {
+    const headword = response.data[0].hwi.hw;
 
-  const matchingDefinitions = response.data.filter(
-    item => item.hwi.hw === headword
-  );
+    const matchingDefinitions = response.data.filter(
+      item => item.hwi.hw === headword
+    );
 
-  const more = matchingDefinitions.map(
-    item => `${item.fl} = ${item.shortdef[0]}`
-  );
-
-  console.log(headword + ": " + more);
+    const more = matchingDefinitions.map(item =>
+      item.fl ? `${item.fl} = ${item.shortdef[0]}` : `${item.cxs[0].cxl}`
+    );
+    console.log(response.data);
+    console.log(headword + ": " + more);
+  }
 };
 
 export default getDefinition;
