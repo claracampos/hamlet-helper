28a38b102580d34afbeb4e5858866ac676dda095
claracampos <camposclr@gmail.com>
2020-02-01 13:48:01 -0400

    added logSynonym function (draft)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
+import logSynonym from "./components/logSynonym";
 
 function App() {
   return (
     <div className="App">
-      <p>example</p>
+      <p onClick={logSynonym}>
+        <span>This</span> <span>is</span> <span>a</span> <span>test</span>.
+      </p>
     </div>
   );
 }
