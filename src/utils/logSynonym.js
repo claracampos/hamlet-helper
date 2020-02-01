2980592540ea070f940d38f31142b7f4c34ef63b
claracampos <camposclr@gmail.com>
2020-02-01 14:39:56 -0400

    implemented getTopSynonyms function

--- src/utils/logSynonym.js
+++ src/utils/logSynonym.js
@@ -1,11 +1,12 @@
 import axios from "axios";
+import getTopSynonyms from "./getTopSynonyms";
 
 const logSynonym = async e => {
   const word = e.target.innerText.toLowerCase();
   try {
     const response = await axios.get(`https://www.dictionaryapi.com/api/v3/references/thesaurus/json/${word}?key=${process.env.REACT_APP_KEY}
       `);
-    console.log(response.data[0].meta.syns[0]);
+    getTopSynonyms(response);
   } catch (error) {
     console.error(error);
   }
