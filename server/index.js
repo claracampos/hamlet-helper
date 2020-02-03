e3cd95dc57a27048fff3f928288019342fc70266
claracampos <camposclr@gmail.com>
2020-02-02 21:42:09 -0400

    removed server from project (setting it up separately)

--- server/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-const port = process.env.port || 5000;
-
-app.get("/", (req, res) => {
-  res.send({ test: 123 });
-});
-app.listen(port);
