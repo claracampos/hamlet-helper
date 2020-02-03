fae542cf73cc156a847b846fd3315ed853d9cc65
claracampos <camposclr@gmail.com>
2020-02-02 20:05:41 -0400

    created a homepage

---
+++ src/pages/Homepage.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Homepage = () => (
+  <div>
+    <h1>Hamlet Helper</h1>
+    <h2>Description here</h2>
+    <Link>Dramatis Personae</Link>
+    <p>ACT I</p>
+    <Link>Scene I. Elsinore. A platform before the Castle.</Link>
+    <Link>Scene II. Elsinore. A room of state in the Castle</Link>
+    <Link>Scene III. A room in Polonius’s house.</Link>
+    <Link>Scene IV. The platform.</Link>
+    <Link>Scene V. A more remote part of the Castle.</Link>
+    <p>ACT II</p>
+    <Link>Scene I. A room in Polonius’s house.</Link>
+    <Link>Scene II. A room in the Castle.</Link>
+    <p>ACT III</p>
+    <Link>Scene I. A room in the Castle.</Link>
+    <Link>Scene II. A hall in the Castle.</Link>
+    <Link>Scene III. A room in the Castle.</Link>
+    <Link>Scene IV. Another room in the Castle.</Link>
+    <p>ACT IV</p>
+    <Link>Scene I. A room in the Castle.</Link>
+    <Link>Scene II. Another room in the Castle.</Link>
+    <Link>Scene III. Another room in the Castle.</Link>
+    <Link>Scene IV. A plain in Denmark.</Link>
+    <Link>Scene V. Elsinore. A room in the Castle.</Link>
+    <Link>Scene VI. Another room in the Castle.</Link>
+    <Link>Scene VII. Another room in the Castle.</Link>
+    <p>ACT V</p>
+    <Link>Scene I. A churchyard.</Link>
+    <Link>Scene II. A hall in the Castle.</Link>
+  </div>
+);
+
+export default Homepage;
