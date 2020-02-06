a169781bd6e15824d87f384324c254738290c220
claracampos <camposclr@gmail.com>
2020-02-06 12:22:48 -0400

    added state and rendering definitions in the page

--- src/pages/TextPage.js
+++ src/pages/TextPage.js
@@ -1,15 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import getDefinition from "../utils/getDefinition";
 
-const TextPage = props => (
-  <div className="d-flex flex-column vh-90">
-    <div className="main-text-container overflow-auto">
-      <h2>{props.title}</h2>
-      {props.scene}
+const TextPage = props => {
+  const [dictionary, setDictionary] = useState({
+    word: "",
+    definition: ""
+  });
+
+  return (
+    <div className="d-flex flex-column vh-100">
+      <div className="main-text-container overflow-auto">
+        <h2>{props.title}</h2>
+        <div
+          onClick={e => {
+            getDefinition(e, setDictionary);
+          }}
+        >
+          {props.scene}
+        </div>
+      </div>
+      <footer className="footer mt-auto dictionary-container overflow-auto bg-warning">
+        <p>{dictionary.definition}</p>
+        {dictionary.definition && (
+          <a
+            href={`https://www.merriam-webster.com/dictionary/${dictionary.word}`}
+            target="blank"
+          >
+            View more
+          </a>
+        )}
+      </footer>
     </div>
-    <footer className="footer mt-auto dictionary-container overflow-auto">
-      test
-    </footer>
-  </div>
-);
+  );
+};
 
 export default TextPage;
