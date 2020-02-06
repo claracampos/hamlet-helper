35be3c865abc565d21b5dbeba09f516b605d73ec
claracampos <camposclr@gmail.com>
2020-02-06 17:42:43 -0400

    added act 3, scene 1

---
+++ src/text/act3/Act3Scene1.js
@@ -0,0 +1,316 @@
+import React from "react";
+
+const Act3Scene1 = () => (
+  <div>
+    <h2>ACT III</h2>
+    <h3>SCENE I. A room in the Castle.</h3>
+    <p className="stage-direction">
+      Enter King, Queen, Polonius, Ophelia, Rosencrantz and Guildenstern.
+    </p>
+    <p className="character-name">KING.</p>
+    <p className="word-split">And can you by no drift of circumstance</p>
+    <p className="word-split">Get from him why he puts on this confusion,</p>
+    <p className="word-split">Grating so harshly all his days of quiet</p>
+    <p className="word-split">With turbulent and dangerous lunacy?</p>
+    <p className="character-name">ROSENCRANTZ.</p>
+    <p className="word-split">He does confess he feels himself distracted,</p>
+    <p className="word-split">But from what cause he will by no means speak.</p>
+    <p className="character-name">GUILDENSTERN.</p>
+    <p className="word-split">Nor do we find him forward to be sounded,</p>
+    <p className="word-split">But with a crafty madness keeps aloof</p>
+    <p className="word-split">When we would bring him on to some confession</p>
+    <p className="word-split">Of his true state.</p>
+    <p className="character-name">QUEEN.</p>
+    <p className="word-split">Did he receive you well?</p>
+    <p className="character-name">ROSENCRANTZ.</p>
+    <p className="word-split">Most like a gentleman.</p>
+    <p className="character-name">GUILDENSTERN.</p>
+    <p className="word-split">But with much forcing of his disposition.</p>
+    <p className="character-name">ROSENCRANTZ.</p>
+    <p className="word-split">Niggard of question, but of our demands,</p>
+    <p className="word-split">Most free in his reply.</p>
+    <p className="character-name">QUEEN.</p>
+    <p className="word-split">Did you assay him to any pastime?</p>
+    <p className="character-name">ROSENCRANTZ.</p>
+    <p className="word-split">Madam, it so fell out that certain players</p>
+    <p className="word-split">
+      We o'er-raught on the way. Of these we told him,
+    </p>
+    <p className="word-split">And there did seem in him a kind of joy</p>
+    <p className="word-split">To hear of it. They are about the court,</p>
+    <p className="word-split">And, as I think, they have already order</p>
+    <p className="word-split">This night to play before him.</p>
+    <p className="character-name">POLONIUS.</p>
+    <p className="word-split">'Tis most true;</p>
+    <p className="word-split">And he beseech'd me to entreat your Majesties</p>
+    <p className="word-split">To hear and see the matter.</p>
+    <p className="character-name">KING.</p>
+    <p className="word-split">With all my heart; and it doth much content me</p>
+    <p className="word-split">To hear him so inclin'd.</p>
+    <p className="word-split">Good gentlemen, give him a further edge,</p>
+    <p className="word-split">And drive his purpose on to these delights.</p>
+    <p className="character-name">ROSENCRANTZ.</p>
+    <p className="word-split">We shall, my lord.</p>
+    <p className="stage-direction">[Exeunt Rosencrantz and Guildenstern.]</p>
+    <p className="character-name">KING.</p>
+    <p className="word-split">Sweet Gertrude, leave us too,</p>
+    <p className="word-split">For we have closely sent for Hamlet hither,</p>
+    <p className="word-split">That he, as 'twere by accident, may here</p>
+    <p className="word-split">Affront Ophelia.</p>
+    <p className="word-split">Her father and myself, lawful espials,</p>
+    <p className="word-split">Will so bestow ourselves that, seeing unseen,</p>
+    <p className="word-split">We may of their encounter frankly judge,</p>
+    <p className="word-split">And gather by him, as he is behav'd,</p>
+    <p className="word-split">If't be th'affliction of his love or no</p>
+    <p className="word-split">That thus he suffers for.</p>
+    <p className="character-name">QUEEN.</p>
+    <p className="word-split">I shall obey you.</p>
+    <p className="word-split">And for your part, Ophelia, I do wish</p>
+    <p className="word-split">That your good beauties be the happy cause</p>
+    <p className="word-split">
+      Of Hamlet's wildness: so shall I hope your virtues
+    </p>
+    <p className="word-split">Will bring him to his wonted way again,</p>
+    <p className="word-split">To both your honours.</p>
+    <p className="character-name">OPHELIA.</p>
+    <p className="word-split">Madam, I wish it may.</p>
+    <p className="stage-direction">[Exit Queen.]</p>
+    <p className="character-name">POLONIUS.</p>
+    <p className="word-split">
+      Ophelia, walk you here. -- Gracious, so please you,
+    </p>
+    <p className="word-split">
+      We will bestow ourselves. -- [To Ophelia.] Read on this book,
+    </p>
+    <p className="word-split">That show of such an exercise may colour</p>
+    <p className="word-split">
+      Your loneliness. -- We are oft to blame in this,
+    </p>
+    <p className="word-split">
+      'Tis too much prov'd, that with devotion's visage
+    </p>
+    <p className="word-split">And pious action we do sugar o'er</p>
+    <p className="word-split">The devil himself.</p>
+    <p className="character-name">KING.</p>
+    <p className="word-split">[Aside.] O 'tis too true!</p>
+    <p className="word-split">
+      How smart a lash that speech doth give my conscience!
+    </p>
+    <p className="word-split">
+      The harlot's cheek, beautied with plastering art,
+    </p>
+    <p className="word-split">Is not more ugly to the thing that helps it</p>
+    <p className="word-split">Than is my deed to my most painted word.</p>
+    <p className="word-split">O heavy burden!</p>
+    <p className="character-name">POLONIUS.</p>
+    <p className="word-split">I hear him coming. Let's withdraw, my lord.</p>
+    <p className="stage-direction">[Exeunt King and Polonius.]</p>
+    <p className="stage-direction"> Enter Hamlet.</p>
+    <p className="character-name">HAMLET.</p>
+    <p className="word-split">To be, or not to be, that is the question:</p>
+    <p className="word-split">Whether 'tis nobler in the mind to suffer</p>
+    <p className="word-split">The slings and arrows of outrageous fortune,</p>
+    <p className="word-split">Or to take arms against a sea of troubles,</p>
+    <p className="word-split">And by opposing end them? To die -- to sleep,</p>
+    <p className="word-split">No more; and by a sleep to say we end</p>
+    <p className="word-split">
+      The heart-ache, and the thousand natural shocks
+    </p>
+    <p className="word-split">That flesh is heir to: 'tis a consummation</p>
+    <p className="word-split">Devoutly to be wish'd. To die, to sleep.</p>
+    <p className="word-split">
+      To sleep, perchance to dream -- ay, there's the rub,
+    </p>
+    <p className="word-split">
+      For in that sleep of death what dreams may come,
+    </p>
+    <p className="word-split">When we have shuffled off this mortal coil,</p>
+    <p className="word-split">Must give us pause. There's the respect</p>
+    <p className="word-split">That makes calamity of so long life.</p>
+    <p className="word-split">
+      For who would bear the whips and scorns of time,
+    </p>
+    <p className="word-split">
+      The oppressor's wrong, the proud man's contumely,
+    </p>
+    <p className="word-split">The pangs of dispriz'd love, the law's delay,</p>
+    <p className="word-split">The insolence of office, and the spurns</p>
+    <p className="word-split">That patient merit of the unworthy takes,</p>
+    <p className="word-split">When he himself might his quietus make</p>
+    <p className="word-split">
+      With a bare bodkin? Who would these fardels bear,
+    </p>
+    <p className="word-split">To grunt and sweat under a weary life,</p>
+    <p className="word-split">But that the dread of something after death,</p>
+    <p className="word-split">The undiscover'd country, from whose bourn</p>
+    <p className="word-split">No traveller returns, puzzles the will,</p>
+    <p className="word-split">And makes us rather bear those ills we have</p>
+    <p className="word-split">Than fly to others that we know not of?</p>
+    <p className="word-split">Thus conscience does make cowards of us all,</p>
+    <p className="word-split">And thus the native hue of resolution</p>
+    <p className="word-split">
+      Is sicklied o'er with the pale cast of thought,
+    </p>
+    <p className="word-split">And enterprises of great pith and moment,</p>
+    <p className="word-split">With this regard their currents turn awry</p>
+    <p className="word-split">And lose the name of action. Soft you now,</p>
+    <p className="word-split">The fair Ophelia! Nymph, in thy orisons</p>
+    <p className="word-split">Be all my sins remember'd.</p>
+    <p className="character-name">OPHELIA.</p>
+    <p className="word-split">Good my lord,</p>
+    <p className="word-split">How does your honour for this many a day?</p>
+    <p className="character-name">HAMLET.</p>
+    <p className="word-split">I humbly thank you; well, well, well.</p>
+    <p className="character-name">OPHELIA.</p>
+    <p className="word-split">My lord, I have remembrances of yours</p>
+    <p className="word-split">That I have longed long to re-deliver.</p>
+    <p className="word-split">I pray you, now receive them.</p>
+    <p className="character-name">HAMLET.</p>
+    <p className="word-split">No, not I.</p>
+    <p className="word-split">I never gave you aught.</p>
+    <p className="character-name">OPHELIA.</p>
+    <p className="word-split">My honour'd lord, you know right well you did,</p>
+    <p className="word-split">
+      And with them words of so sweet breath compos'd
+    </p>
+    <p className="word-split">
+      As made the things more rich; their perfume lost,
+    </p>
+    <p className="word-split">Take these again; for to the noble mind</p>
+    <p className="word-split">Rich gifts wax poor when givers prove unkind.</p>
+    <p className="word-split">There, my lord.</p>
+    <p className="character-name">HAMLET.</p>
+    <p className="word-split">Ha, ha! Are you honest?</p>
+    <p className="character-name">OPHELIA.</p>
+    <p className="word-split">My lord?</p>
+    <p className="character-name">HAMLET.</p>
+    <p className="word-split">Are you fair?</p>
+    <p className="character-name">OPHELIA.</p>
+    <p className="word-split">What means your lordship?</p>
+    <p className="character-name">HAMLET.</p>
+    <p className="word-split">
+      That if you be honest and fair, your honesty should admit no discourse to
+      your beauty.
+    </p>
+    <p className="character-name">OPHELIA.</p>
+    <p className="word-split">
+      Could beauty, my lord, have better commerce than with honesty?
+    </p>
+    <p className="character-name">HAMLET.</p>
+    <p className="word-split">
+      Ay, truly; for the power of beauty will sooner transform honesty from what
+      it is to a bawd than the force of honesty can translate beauty into his
+      likeness. This was sometime a paradox, but now the time gives it proof. I
+      did love you once.
+    </p>
+    <p className="character-name">OPHELIA.</p>
+    <p className="word-split">Indeed, my lord, you made me believe so.</p>
+    <p className="character-name">HAMLET.</p>
+    <p className="word-split">
+      You should not have believed me; for virtue cannot so inoculate our old
+      stock but we shall relish of it. I loved you not.
+    </p>
+    <p className="character-name">OPHELIA.</p>
+    <p className="word-split">I was the more deceived.</p>
+    <p className="character-name">HAMLET.</p>
+    <p className="word-split">
+      Get thee to a nunnery. Why wouldst thou be a breeder of sinners? I am
+      myself indifferent honest; but yet I could accuse me of such things that
+      it were better my mother had not borne me. I am very proud, revengeful,
+      ambitious, with more offences at my beck than I have thoughts to put them
+      in, imagination to give them shape, or time to act them in. What should
+      such fellows as I do crawling between earth and heaven? We are arrant
+      knaves all, believe none of us. Go thy ways to a nunnery. Where's your
+      father?
+    </p>
+    <p className="character-name">OPHELIA.</p>
+    <p className="word-split">At home, my lord.</p>
+    <p className="character-name">HAMLET.</p>
+    <p className="word-split">
+      Let the doors be shut upon him, that he may play the fool nowhere but in's
+      own house. Farewell.
+    </p>
+    <p className="character-name">OPHELIA.</p>
+    <p className="word-split">O help him, you sweet heavens!</p>
+    <p className="character-name">HAMLET.</p>
+    <p className="word-split">
+      If thou dost marry, I'll give thee this plague for thy dowry. Be thou as
+      chaste as ice, as pure as snow, thou shalt not escape calumny. Get thee to
+      a nunnery, go: farewell. Or if thou wilt needs marry, marry a fool; for
+      wise men know well enough what monsters you make of them. To a nunnery,
+      go; and quickly too. Farewell.
+    </p>
+    <p className="character-name">OPHELIA.</p>
+    <p className="word-split">O heavenly powers, restore him!</p>
+    <p className="character-name">HAMLET.</p>
+    <p className="word-split">
+      I have heard of your paintings too, well enough. God hath given you one
+      face, and you make yourselves another. You jig, you amble, and you lisp,
+      and nickname God's creatures, and make your wantonness your ignorance. Go
+      to, I'll no more on't, it hath made me mad. I say, we will have no more
+      marriages. Those that are married already, all but one, shall live; the
+      rest shall keep as they are. To a nunnery, go.
+    </p>
+    <p className="stage-direction">[Exit.]</p>
+    <p className="character-name">OPHELIA.</p>
+    <p className="word-split">O, what a noble mind is here o'erthrown!</p>
+    <p className="word-split">
+      The courtier's, soldier's, scholar's, eye, tongue, sword,
+    </p>
+    <p className="word-split">Th'expectancy and rose of the fair state,</p>
+    <p className="word-split">The glass of fashion and the mould of form,</p>
+    <p className="word-split">
+      Th'observ'd of all observers, quite, quite down!
+    </p>
+    <p className="word-split">And I, of ladies most deject and wretched,</p>
+    <p className="word-split">That suck'd the honey of his music vows,</p>
+    <p className="word-split">Now see that noble and most sovereign reason,</p>
+    <p className="word-split">
+      Like sweet bells jangled out of tune and harsh,
+    </p>
+    <p className="word-split">That unmatch'd form and feature of blown youth</p>
+    <p className="word-split">Blasted with ecstasy. O woe is me,</p>
+    <p className="word-split">T'have seen what I have seen, see what I see.</p>
+    <p className="stage-direction"> Enter King and Polonius.</p>
+    <p className="character-name">KING.</p>
+    <p className="word-split">Love? His affections do not that way tend,</p>
+    <p className="word-split">
+      Nor what he spake, though it lack'd form a little,
+    </p>
+    <p className="word-split">
+      Was not like madness. There's something in his soul
+    </p>
+    <p className="word-split">O'er which his melancholy sits on brood,</p>
+    <p className="word-split">And I do doubt the hatch and the disclose</p>
+    <p className="word-split">Will be some danger, which for to prevent,</p>
+    <p className="word-split">I have in quick determination</p>
+    <p className="word-split">
+      Thus set it down: he shall with speed to England
+    </p>
+    <p className="word-split">For the demand of our neglected tribute:</p>
+    <p className="word-split">Haply the seas and countries different,</p>
+    <p className="word-split">With variable objects, shall expel</p>
+    <p className="word-split">This something settled matter in his heart,</p>
+    <p className="word-split">Whereon his brains still beating puts him thus</p>
+    <p className="word-split">From fashion of himself. What think you on't?</p>
+    <p className="character-name">POLONIUS.</p>
+    <p className="word-split">It shall do well. But yet do I believe</p>
+    <p className="word-split">The origin and commencement of his grief</p>
+    <p className="word-split">Sprung from neglected love. How now, Ophelia?</p>
+    <p className="word-split">You need not tell us what Lord Hamlet said,</p>
+    <p className="word-split">We heard it all. My lord, do as you please,</p>
+    <p className="word-split">But if you hold it fit, after the play,</p>
+    <p className="word-split">Let his queen mother all alone entreat him</p>
+    <p className="word-split">To show his grief, let her be round with him,</p>
+    <p className="word-split">And I'll be plac'd, so please you, in the ear</p>
+    <p className="word-split">Of all their conference. If she find him not,</p>
+    <p className="word-split">To England send him; or confine him where</p>
+    <p className="word-split">Your wisdom best shall think.</p>
+    <p className="character-name">KING.</p>
+    <p className="word-split">It shall be so.</p>
+    <p className="word-split">Madness in great ones must not unwatch'd go.</p>
+    <p className="stage-direction">[Exeunt.]</p>
+  </div>
+);
+
+export default Act3Scene1;
