159019665601e4e92e8a8b3d472df639ebaefd51
claracampos <camposclr@gmail.com>
2020-02-06 17:43:12 -0400

    added act 4, scene 2

---
+++ src/text/act4/Act4Scene2.js
@@ -0,0 +1,72 @@
+import React from "react";
+
+const Act4Scene2 = () => (
+  <div>
+    <h2>ACT IV</h2>
+    <h3>SCENE II. Another room in the Castle.</h3>
+    <p className="stage-direction"> Enter Hamlet.</p>
+    <p className="character-name">HAMLET.</p>
+    <p className="word-split">Safely stowed.</p>
+    <p className="character-name">ROSENCRANTZ and GUILDENSTERN.</p>
+    <p className="word-split">[Within.] Hamlet! Lord Hamlet!</p>
+    <p className="character-name">HAMLET.</p>
+    <p className="word-split">
+      What noise? Who calls on Hamlet? O, here they come.
+    </p>
+    <p className="stage-direction"> Enter Rosencrantz and Guildenstern.</p>
+    <p className="character-name">ROSENCRANTZ.</p>
+    <p className="word-split">
+      What have you done, my lord, with the dead body?
+    </p>
+    <p className="character-name">HAMLET.</p>
+    <p className="word-split">Compounded it with dust, whereto 'tis kin.</p>
+    <p className="character-name">ROSENCRANTZ.</p>
+    <p className="word-split">
+      Tell us where 'tis, that we may take it thence,
+    </p>
+    <p className="word-split">And bear it to the chapel.</p>
+    <p className="character-name">HAMLET.</p>
+    <p className="word-split">Do not believe it.</p>
+    <p className="character-name">ROSENCRANTZ.</p>
+    <p className="word-split">Believe what?</p>
+    <p className="character-name">HAMLET.</p>
+    <p className="word-split">
+      That I can keep your counsel, and not mine own. Besides, to be demanded of
+      a sponge -- what replication should be made by the son of a king?
+    </p>
+    <p className="character-name">ROSENCRANTZ.</p>
+    <p className="word-split">Take you me for a sponge, my lord?</p>
+    <p className="character-name">HAMLET.</p>
+    <p className="word-split">
+      Ay, sir; that soaks up the King's countenance, his rewards, his
+      authorities. But such officers do the King best service in the end: he
+      keeps them, like an ape, in the corner of his jaw; first mouthed, to be
+      last swallowed: when he needs what you have gleaned, it is but squeezing
+      you, and, sponge, you shall be dry again.
+    </p>
+    <p className="character-name">ROSENCRANTZ.</p>
+    <p className="word-split">I understand you not, my lord.</p>
+    <p className="character-name">HAMLET.</p>
+    <p className="word-split">
+      I am glad of it. A knavish speech sleeps in a foolish ear.
+    </p>
+    <p className="character-name">ROSENCRANTZ.</p>
+    <p className="word-split">
+      My lord, you must tell us where the body is and go with us to the King.
+    </p>
+    <p className="character-name">HAMLET.</p>
+    <p className="word-split">
+      The body is with the King, but the King is not with the body. The King is
+      a thing --
+    </p>
+    <p className="character-name">GUILDENSTERN.</p>
+    <p className="word-split">A thing, my lord!</p>
+    <p className="character-name">HAMLET.</p>
+    <p className="word-split">
+      Of nothing. Bring me to him. Hide fox, and all after.
+    </p>
+    <p className="stage-direction">[Exeunt.]</p>
+  </div>
+);
+
+export default Act4Scene2;
