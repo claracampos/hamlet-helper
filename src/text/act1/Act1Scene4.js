f0338fcf71d5214d784622f0743c1f020f453bbf
claracampos <camposclr@gmail.com>
2020-02-06 09:42:21 -0400

    added act 1, scene 4 text

---
+++ src/text/act1/Act1Scene4.js
@@ -0,0 +1,393 @@
+import React from "react";
+
+const Act1Scene4 = () => (
+  <div>
+    <h4>
+      <b>SCENE IV. The platform.</b>
+    </h4>
+
+    <p>
+      <i>Enter Hamlet, Horatio and Marcellus.</i>
+    </p>
+
+    <p>
+      HAMLET.
+      <br />
+      The air bites shrewdly; it is very cold.
+      <br />
+    </p>
+
+    <p>
+      HORATIO.
+      <br />
+      It is a nipping and an eager air.
+      <br />
+    </p>
+
+    <p>
+      HAMLET.
+      <br />
+      What hour now?
+      <br />
+    </p>
+
+    <p>
+      HORATIO.
+      <br />
+      I think it lacks of twelve.
+      <br />
+    </p>
+
+    <p>
+      MARCELLUS.
+      <br />
+      No, it is struck.
+      <br />
+    </p>
+
+    <p>
+      HORATIO.
+      <br />
+      Indeed? I heard it not. It then draws near the season
+      <br />
+      Wherein the spirit held his wont to walk.
+      <br />
+    </p>
+
+    <p>
+      <i>[A flourish of trumpets, and ordnance shot off within.]</i>
+    </p>
+
+    <p>What does this mean, my lord?</p>
+
+    <p>
+      HAMLET.
+      <br />
+      The King doth wake tonight and takes his rouse,
+      <br />
+      Keeps wassail, and the swaggering upspring reels;
+      <br />
+      And as he drains his draughts of Rhenish down,
+      <br />
+      The kettle-drum and trumpet thus bray out
+      <br />
+      The triumph of his pledge.
+      <br />
+    </p>
+
+    <p>
+      HORATIO.
+      <br />
+      Is it a custom?
+      <br />
+    </p>
+
+    <p>
+      HAMLET.
+      <br />
+      Ay marry is&rsquo;t;
+      <br />
+      And to my mind, though I am native here,
+      <br />
+      And to the manner born, it is a custom
+      <br />
+      More honour&rsquo;d in the breach than the observance.
+      <br />
+      This heavy-headed revel east and west
+      <br />
+      Makes us traduc&rsquo;d and tax&rsquo;d of other nations:
+      <br />
+      They clepe us drunkards, and with swinish phrase
+      <br />
+      Soil our addition; and indeed it takes
+      <br />
+      From our achievements, though perform&rsquo;d at height,
+      <br />
+      The pith and marrow of our attribute.
+      <br />
+      So oft it chances in particular men
+      <br />
+      That for some vicious mole of nature in them,
+      <br />
+      As in their birth, wherein they are not guilty,
+      <br />
+      Since nature cannot choose his origin,
+      <br />
+      By their o&rsquo;ergrowth of some complexion,
+      <br />
+      Oft breaking down the pales and forts of reason;
+      <br />
+      Or by some habit, that too much o&rsquo;erleavens
+      <br />
+      The form of plausive manners;&mdash;that these men,
+      <br />
+      Carrying, I say, the stamp of one defect,
+      <br />
+      Being Nature&rsquo;s livery or Fortune&rsquo;s star,&mdash;
+      <br />
+      His virtues else,&mdash;be they as pure as grace,
+      <br />
+      As infinite as man may undergo,
+      <br />
+      Shall in the general censure take corruption
+      <br />
+      From that particular fault. The dram of evil
+      <br />
+      Doth all the noble substance often doubt
+      <br />
+      To his own scandal.
+      <br />
+    </p>
+
+    <p>
+      HORATIO.
+      <br />
+      Look, my lord, it comes!
+      <br />
+    </p>
+
+    <p>
+      <i>Enter Ghost.</i>
+    </p>
+
+    <p>
+      HAMLET.
+      <br />
+      Angels and ministers of grace defend us!
+      <br />
+      Be thou a spirit of health or goblin damn&rsquo;d,
+      <br />
+      Bring with thee airs from heaven or blasts from hell,
+      <br />
+      Be thy intents wicked or charitable,
+      <br />
+      Thou com&rsquo;st in such a questionable shape
+      <br />
+      That I will speak to thee. I&rsquo;ll call thee Hamlet,
+      <br />
+      King, father, royal Dane. O, answer me!
+      <br />
+      Let me not burst in ignorance; but tell
+      <br />
+      Why thy canoniz&rsquo;d bones, hearsed in death,
+      <br />
+      Have burst their cerements; why the sepulchre,
+      <br />
+      Wherein we saw thee quietly inurn&rsquo;d,
+      <br />
+      Hath op&rsquo;d his ponderous and marble jaws
+      <br />
+      To cast thee up again! What may this mean,
+      <br />
+      That thou, dead corse, again in complete steel,
+      <br />
+      Revisit&rsquo;st thus the glimpses of the moon,
+      <br />
+      Making night hideous, and we fools of nature
+      <br />
+      So horridly to shake our disposition
+      <br />
+      With thoughts beyond the reaches of our souls?
+      <br />
+      Say, why is this? Wherefore? What should we do?
+      <br />
+    </p>
+
+    <p>
+      <i>Ghost beckons Hamlet.</i>
+    </p>
+
+    <p>
+      HORATIO.
+      <br />
+      It beckons you to go away with it,
+      <br />
+      As if it some impartment did desire
+      <br />
+      To you alone.
+      <br />
+    </p>
+
+    <p>
+      MARCELLUS.
+      <br />
+      Look with what courteous action
+      <br />
+      It waves you to a more removed ground.
+      <br />
+      But do not go with it.
+      <br />
+    </p>
+
+    <p>
+      HORATIO.
+      <br />
+      No, by no means.
+      <br />
+    </p>
+
+    <p>
+      HAMLET.
+      <br />
+      It will not speak; then will I follow it.
+      <br />
+    </p>
+
+    <p>
+      HORATIO.
+      <br />
+      Do not, my lord.
+      <br />
+    </p>
+
+    <p>
+      HAMLET.
+      <br />
+      Why, what should be the fear?
+      <br />
+      I do not set my life at a pin&rsquo;s fee;
+      <br />
+      And for my soul, what can it do to that,
+      <br />
+      Being a thing immortal as itself?
+      <br />
+      It waves me forth again. I&rsquo;ll follow it.
+      <br />
+    </p>
+
+    <p>
+      HORATIO.
+      <br />
+      What if it tempt you toward the flood, my lord,
+      <br />
+      Or to the dreadful summit of the cliff
+      <br />
+      That beetles o&rsquo;er his base into the sea,
+      <br />
+      And there assume some other horrible form
+      <br />
+      Which might deprive your sovereignty of reason,
+      <br />
+      And draw you into madness? Think of it.
+      <br />
+      The very place puts toys of desperation,
+      <br />
+      Without more motive, into every brain
+      <br />
+      That looks so many fadoms to the sea
+      <br />
+      And hears it roar beneath.
+      <br />
+    </p>
+
+    <p>
+      HAMLET.
+      <br />
+      It waves me still.
+      <br />
+      Go on, I&rsquo;ll follow thee.
+      <br />
+    </p>
+
+    <p>
+      MARCELLUS.
+      <br />
+      You shall not go, my lord.
+      <br />
+    </p>
+
+    <p>
+      HAMLET.
+      <br />
+      Hold off your hands.
+      <br />
+    </p>
+
+    <p>
+      HORATIO.
+      <br />
+      Be rul&rsquo;d; you shall not go.
+      <br />
+    </p>
+
+    <p>
+      HAMLET.
+      <br />
+      My fate cries out,
+      <br />
+      And makes each petty artery in this body
+      <br />
+      As hardy as the Nemean lion&rsquo;s nerve.
+      <br />
+    </p>
+
+    <p>
+      [<i>Ghost beckons.</i>]
+    </p>
+
+    <p>Still am I call&rsquo;d. Unhand me, gentlemen.</p>
+
+    <p>
+      [<i>Breaking free from them.</i>]
+    </p>
+
+    <p>
+      By heaven, I&rsquo;ll make a ghost of him that lets me.
+      <br />
+      I say, away!&mdash;Go on, I&rsquo;ll follow thee.
+      <br />
+    </p>
+
+    <p>
+      <i>Exeunt Ghost and Hamlet.</i>
+    </p>
+
+    <p>
+      HORATIO.
+      <br />
+      He waxes desperate with imagination.
+      <br />
+    </p>
+
+    <p>
+      MARCELLUS.
+      <br />
+      Let&rsquo;s follow; &rsquo;tis not fit thus to obey him.
+      <br />
+    </p>
+
+    <p>
+      HORATIO.
+      <br />
+      Have after. To what issue will this come?
+      <br />
+    </p>
+
+    <p>
+      MARCELLUS.
+      <br />
+      Something is rotten in the state of Denmark.
+      <br />
+    </p>
+
+    <p>
+      HORATIO.
+      <br />
+      Heaven will direct it.
+      <br />
+    </p>
+
+    <p>
+      MARCELLUS.
+      <br />
+      Nay, let&rsquo;s follow him.
+      <br />
+    </p>
+
+    <p>
+      <i>Exeunt.</i>
+    </p>
+  </div>
+);
+
+export default Act1Scene4;
