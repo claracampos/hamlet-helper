ebf9e93a985eeabc155d13f59a77a9ba76c54f3e
claracampos <camposclr@gmail.com>
2020-02-06 09:35:35 -0400

    added act 1, scene 3 text

---
+++ src/text/act1/Act1Scene3.js
@@ -0,0 +1,442 @@
+import React from "react";
+
+const Act1Scene3 = () => (
+  <div>
+    <h3>SCENE III. A room in Polonius&rsquo;s house.</h3>
+
+    <p>
+      <i>Enter Laertes and Ophelia.</i>
+    </p>
+
+    <p>
+      LAERTES.
+      <br />
+      My necessaries are embark&rsquo;d. Farewell.
+      <br />
+      And, sister, as the winds give benefit
+      <br />
+      And convoy is assistant, do not sleep,
+      <br />
+      But let me hear from you.
+      <br />
+    </p>
+
+    <p>
+      OPHELIA.
+      <br />
+      Do you doubt that?
+      <br />
+    </p>
+
+    <p>
+      LAERTES.
+      <br />
+      For Hamlet, and the trifling of his favour,
+      <br />
+      Hold it a fashion and a toy in blood;
+      <br />
+      A violet in the youth of primy nature,
+      <br />
+      Forward, not permanent, sweet, not lasting;
+      <br />
+      The perfume and suppliance of a minute;
+      <br />
+      No more.
+      <br />
+    </p>
+
+    <p>
+      OPHELIA.
+      <br />
+      No more but so?
+      <br />
+    </p>
+
+    <p>
+      LAERTES.
+      <br />
+      Think it no more.
+      <br />
+      For nature crescent does not grow alone
+      <br />
+      In thews and bulk; but as this temple waxes,
+      <br />
+      The inward service of the mind and soul
+      <br />
+      Grows wide withal. Perhaps he loves you now,
+      <br />
+      And now no soil nor cautel doth besmirch
+      <br />
+      The virtue of his will; but you must fear,
+      <br />
+      His greatness weigh&rsquo;d, his will is not his own;
+      <br />
+      For he himself is subject to his birth:
+      <br />
+      He may not, as unvalu&rsquo;d persons do,
+      <br />
+      Carve for himself; for on his choice depends
+      <br />
+      The sanctity and health of this whole state;
+      <br />
+      And therefore must his choice be circumscrib&rsquo;d
+      <br />
+      Unto the voice and yielding of that body
+      <br />
+      Whereof he is the head. Then if he says he loves you,
+      <br />
+      It fits your wisdom so far to believe it
+      <br />
+      As he in his particular act and place
+      <br />
+      May give his saying deed; which is no further
+      <br />
+      Than the main voice of Denmark goes withal.
+      <br />
+      Then weigh what loss your honour may sustain
+      <br />
+      If with too credent ear you list his songs,
+      <br />
+      Or lose your heart, or your chaste treasure open
+      <br />
+      To his unmaster&rsquo;d importunity.
+      <br />
+      Fear it, Ophelia, fear it, my dear sister;
+      <br />
+      And keep you in the rear of your affection,
+      <br />
+      Out of the shot and danger of desire.
+      <br />
+      The chariest maid is prodigal enough
+      <br />
+      If she unmask her beauty to the moon.
+      <br />
+      Virtue itself scopes not calumnious strokes:
+      <br />
+      The canker galls the infants of the spring
+      <br />
+      Too oft before their buttons be disclos&rsquo;d,
+      <br />
+      And in the morn and liquid dew of youth
+      <br />
+      Contagious blastments are most imminent.
+      <br />
+      Be wary then, best safety lies in fear.
+      <br />
+      Youth to itself rebels, though none else near.
+      <br />
+    </p>
+
+    <p>
+      OPHELIA.
+      <br />
+      I shall th&rsquo;effect of this good lesson keep
+      <br />
+      As watchman to my heart. But good my brother,
+      <br />
+      Do not as some ungracious pastors do,
+      <br />
+      Show me the steep and thorny way to heaven;
+      <br />
+      Whilst like a puff&rsquo;d and reckless libertine
+      <br />
+      Himself the primrose path of dalliance treads,
+      <br />
+      And recks not his own rede.
+      <br />
+    </p>
+
+    <p>
+      LAERTES.
+      <br />
+      O, fear me not.
+      <br />
+      I stay too long. But here my father comes.
+      <br />
+    </p>
+
+    <p>
+      <i>Enter Polonius.</i>
+    </p>
+
+    <p>
+      A double blessing is a double grace;
+      <br />
+      Occasion smiles upon a second leave.
+      <br />
+    </p>
+
+    <p>
+      POLONIUS.
+      <br />
+      Yet here, Laertes? Aboard, aboard, for shame.
+      <br />
+      The wind sits in the shoulder of your sail,
+      <br />
+      And you are stay&rsquo;d for. There, my blessing with you.
+      <br />
+    </p>
+
+    <p>
+      [<i>Laying his hand on Laertes&rsquo;s head.</i>]
+    </p>
+
+    <p>
+      And these few precepts in thy memory
+      <br />
+      Look thou character. Give thy thoughts no tongue,
+      <br />
+      Nor any unproportion&rsquo;d thought his act.
+      <br />
+      Be thou familiar, but by no means vulgar.
+      <br />
+      Those friends thou hast, and their adoption tried,
+      <br />
+      Grapple them unto thy soul with hoops of steel;
+      <br />
+      But do not dull thy palm with entertainment
+      <br />
+      Of each new-hatch&rsquo;d, unfledg&rsquo;d comrade. Beware
+      <br />
+      Of entrance to a quarrel; but being in,
+      <br />
+      Bear&rsquo;t that th&rsquo;opposed may beware of thee.
+      <br />
+      Give every man thine ear, but few thy voice:
+      <br />
+      Take each man&rsquo;s censure, but reserve thy judgment.
+      <br />
+      Costly thy habit as thy purse can buy,
+      <br />
+      But not express&rsquo;d in fancy; rich, not gaudy:
+      <br />
+      For the apparel oft proclaims the man;
+      <br />
+      And they in France of the best rank and station
+      <br />
+      Are of a most select and generous chief in that.
+      <br />
+      Neither a borrower nor a lender be:
+      <br />
+      For loan oft loses both itself and friend;
+      <br />
+      And borrowing dulls the edge of husbandry.
+      <br />
+      This above all: to thine own self be true;
+      <br />
+      And it must follow, as the night the day,
+      <br />
+      Thou canst not then be false to any man.
+      <br />
+      Farewell: my blessing season this in thee.
+      <br />
+    </p>
+
+    <p>
+      LAERTES.
+      <br />
+      Most humbly do I take my leave, my lord.
+      <br />
+    </p>
+
+    <p>
+      POLONIUS.
+      <br />
+      The time invites you; go, your servants tend.
+      <br />
+    </p>
+
+    <p>
+      LAERTES.
+      <br />
+      Farewell, Ophelia, and remember well
+      <br />
+      What I have said to you.
+      <br />
+    </p>
+
+    <p>
+      OPHELIA.
+      <br />
+      &rsquo;Tis in my memory lock&rsquo;d,
+      <br />
+      And you yourself shall keep the key of it.
+      <br />
+    </p>
+
+    <p>
+      LAERTES.
+      <br />
+      Farewell.
+      <br />
+    </p>
+
+    <p>
+      <i>Exit.</i>
+    </p>
+
+    <p>
+      POLONIUS.
+      <br />
+      What is&rsquo;t, Ophelia, he hath said to you?
+      <br />
+    </p>
+
+    <p>
+      OPHELIA.
+      <br />
+      So please you, something touching the Lord Hamlet.
+      <br />
+    </p>
+
+    <p>
+      POLONIUS.
+      <br />
+      Marry, well bethought:
+      <br />
+      &rsquo;Tis told me he hath very oft of late
+      <br />
+      Given private time to you; and you yourself
+      <br />
+      Have of your audience been most free and bounteous.
+      <br />
+      If it be so,&mdash;as so &rsquo;tis put on me,
+      <br />
+      And that in way of caution,&mdash;I must tell you
+      <br />
+      You do not understand yourself so clearly
+      <br />
+      As it behoves my daughter and your honour.
+      <br />
+      What is between you? Give me up the truth.
+      <br />
+    </p>
+
+    <p>
+      OPHELIA.
+      <br />
+      He hath, my lord, of late made many tenders
+      <br />
+      Of his affection to me.
+      <br />
+    </p>
+
+    <p>
+      POLONIUS.
+      <br />
+      Affection! Pooh! You speak like a green girl,
+      <br />
+      Unsifted in such perilous circumstance.
+      <br />
+      Do you believe his tenders, as you call them?
+      <br />
+    </p>
+
+    <p>
+      OPHELIA.
+      <br />
+      I do not know, my lord, what I should think.
+      <br />
+    </p>
+
+    <p>
+      POLONIUS.
+      <br />
+      Marry, I&rsquo;ll teach you; think yourself a baby;
+      <br />
+      That you have ta&rsquo;en these tenders for true pay,
+      <br />
+      Which are not sterling. Tender yourself more dearly;
+      <br />
+      Or,&mdash;not to crack the wind of the poor phrase,
+      <br />
+      Roaming it thus,&mdash;you&rsquo;ll tender me a fool.
+      <br />
+    </p>
+
+    <p>
+      OPHELIA.
+      <br />
+      My lord, he hath importun&rsquo;d me with love
+      <br />
+      In honourable fashion.
+      <br />
+    </p>
+
+    <p>
+      POLONIUS.
+      <br />
+      Ay, fashion you may call it; go to, go to.
+      <br />
+    </p>
+
+    <p>
+      OPHELIA.
+      <br />
+      And hath given countenance to his speech, my lord,
+      <br />
+      With almost all the holy vows of heaven.
+      <br />
+    </p>
+
+    <p>
+      POLONIUS.
+      <br />
+      Ay, springes to catch woodcocks. I do know,
+      <br />
+      When the blood burns, how prodigal the soul
+      <br />
+      Lends the tongue vows: these blazes, daughter,
+      <br />
+      Giving more light than heat, extinct in both,
+      <br />
+      Even in their promise, as it is a-making,
+      <br />
+      You must not take for fire. From this time
+      <br />
+      Be something scanter of your maiden presence;
+      <br />
+      Set your entreatments at a higher rate
+      <br />
+      Than a command to parley. For Lord Hamlet,
+      <br />
+      Believe so much in him that he is young;
+      <br />
+      And with a larger tether may he walk
+      <br />
+      Than may be given you. In few, Ophelia,
+      <br />
+      Do not believe his vows; for they are brokers,
+      <br />
+      Not of that dye which their investments show,
+      <br />
+      But mere implorators of unholy suits,
+      <br />
+      Breathing like sanctified and pious bawds,
+      <br />
+      The better to beguile. This is for all.
+      <br />
+      I would not, in plain terms, from this time forth
+      <br />
+      Have you so slander any moment leisure
+      <br />
+      As to give words or talk with the Lord Hamlet.
+      <br />
+      Look to&rsquo;t, I charge you; come your ways.
+      <br />
+    </p>
+
+    <p>
+      OPHELIA.
+      <br />
+      I shall obey, my lord.
+      <br />
+    </p>
+
+    <p>
+      <i>Exeunt.</i>
+    </p>
+  </div>
+);
+
+export default Act1Scene3;
