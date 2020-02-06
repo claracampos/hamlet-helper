d196d34893f8a971c2e6c1139a711b2faf15349d
claracampos <camposclr@gmail.com>
2020-02-06 17:43:21 -0400

    added act 4, scene 4

---
+++ src/text/act4/Act4Scene4.js
@@ -0,0 +1,112 @@
+import React from "react";
+
+const Act4Scene4 = () => (
+  <div>
+    <h2>ACT IV</h2>
+    <h3>SCENE IV. A plain in Denmark.</h3>
+    <p className="stage-direction"> Enter Fortinbras and Forces marching.</p>
+    <p className="character-name">FORTINBRAS.</p>
+    <p className="word-split">Go, Captain, from me greet the Danish king.</p>
+    <p className="word-split">Tell him that by his license, Fortinbras</p>
+    <p className="word-split">Craves the conveyance of a promis'd march</p>
+    <p className="word-split">Over his kingdom. You know the rendezvous.</p>
+    <p className="word-split">If that his Majesty would aught with us,</p>
+    <p className="word-split">We shall express our duty in his eye;</p>
+    <p className="word-split">And let him know so.</p>
+    <p className="character-name">CAPTAIN.</p>
+    <p className="word-split">I will do't, my lord.</p>
+    <p className="character-name">FORTINBRAS.</p>
+    <p className="word-split">Go softly on.</p>
+    <p className="stage-direction">[Exeunt all but the Captain.]</p>
+    <p className="stage-direction">
+      {" "}
+      Enter Hamlet, Rosencrantz, Guildenstern et cetera.
+    </p>
+    <p className="character-name">HAMLET.</p>
+    <p className="word-split">Good sir, whose powers are these?</p>
+    <p className="character-name">CAPTAIN.</p>
+    <p className="word-split">They are of Norway, sir.</p>
+    <p className="character-name">HAMLET.</p>
+    <p className="word-split">How purpos'd, sir, I pray you?</p>
+    <p className="character-name">CAPTAIN.</p>
+    <p className="word-split">Against some part of Poland.</p>
+    <p className="character-name">HAMLET.</p>
+    <p className="word-split">Who commands them, sir?</p>
+    <p className="character-name">CAPTAIN.</p>
+    <p className="word-split">The nephew to old Norway, Fortinbras.</p>
+    <p className="character-name">HAMLET.</p>
+    <p className="word-split">Goes it against the main of Poland, sir,</p>
+    <p className="word-split">Or for some frontier?</p>
+    <p className="character-name">CAPTAIN.</p>
+    <p className="word-split">Truly to speak, and with no addition,</p>
+    <p className="word-split">We go to gain a little patch of ground</p>
+    <p className="word-split">That hath in it no profit but the name.</p>
+    <p className="word-split">To pay five ducats, five, I would not farm it;</p>
+    <p className="word-split">Nor will it yield to Norway or the Pole</p>
+    <p className="word-split">A ranker rate, should it be sold in fee.</p>
+    <p className="character-name">HAMLET.</p>
+    <p className="word-split">Why, then the Polack never will defend it.</p>
+    <p className="character-name">CAPTAIN.</p>
+    <p className="word-split">Yes, it is already garrison'd.</p>
+    <p className="character-name">HAMLET.</p>
+    <p className="word-split">Two thousand souls and twenty thousand ducats</p>
+    <p className="word-split">Will not debate the question of this straw!</p>
+    <p className="word-split">
+      This is th'imposthume of much wealth and peace,
+    </p>
+    <p className="word-split">That inward breaks, and shows no cause without</p>
+    <p className="word-split">Why the man dies. I humbly thank you, sir.</p>
+    <p className="character-name">CAPTAIN.</p>
+    <p className="word-split">God b' wi' you, sir.</p>
+    <p className="stage-direction">[Exit.]</p>
+    <p className="character-name">ROSENCRANTZ.</p>
+    <p className="word-split">Will't please you go, my lord?</p>
+    <p className="character-name">HAMLET.</p>
+    <p className="word-split">I'll be with you straight. Go a little before.</p>
+    <p className="stage-direction">[Exeunt all but Hamlet.]</p>
+    <p className="word-split">How all occasions do inform against me,</p>
+    <p className="word-split">And spur my dull revenge. What is a man</p>
+    <p className="word-split">If his chief good and market of his time</p>
+    <p className="word-split">Be but to sleep and feed? A beast, no more.</p>
+    <p className="word-split">
+      Sure he that made us with such large discourse,
+    </p>
+    <p className="word-split">Looking before and after, gave us not</p>
+    <p className="word-split">That capability and godlike reason</p>
+    <p className="word-split">To fust in us unus'd. Now whether it be</p>
+    <p className="word-split">Bestial oblivion, or some craven scruple</p>
+    <p className="word-split">Of thinking too precisely on th'event, -- </p>
+    <p className="word-split">
+      A thought which, quarter'd, hath but one part wisdom
+    </p>
+    <p className="word-split">And ever three parts coward, -- I do not know</p>
+    <p className="word-split">Why yet I live to say this thing's to do,</p>
+    <p className="word-split">
+      Sith I have cause, and will, and strength, and means
+    </p>
+    <p className="word-split">To do't. Examples gross as earth exhort me,</p>
+    <p className="word-split">Witness this army of such mass and charge,</p>
+    <p className="word-split">Led by a delicate and tender prince,</p>
+    <p className="word-split">Whose spirit, with divine ambition puff'd,</p>
+    <p className="word-split">Makes mouths at the invisible event,</p>
+    <p className="word-split">Exposing what is mortal and unsure</p>
+    <p className="word-split">To all that fortune, death, and danger dare,</p>
+    <p className="word-split">Even for an eggshell. Rightly to be great</p>
+    <p className="word-split">Is not to stir without great argument,</p>
+    <p className="word-split">But greatly to find quarrel in a straw</p>
+    <p className="word-split">When honour's at the stake. How stand I then,</p>
+    <p className="word-split">That have a father kill'd, a mother stain'd,</p>
+    <p className="word-split">Excitements of my reason and my blood,</p>
+    <p className="word-split">And let all sleep, while to my shame I see</p>
+    <p className="word-split">The imminent death of twenty thousand men</p>
+    <p className="word-split">That, for a fantasy and trick of fame,</p>
+    <p className="word-split">Go to their graves like beds, fight for a plot</p>
+    <p className="word-split">Whereon the numbers cannot try the cause,</p>
+    <p className="word-split">Which is not tomb enough and continent</p>
+    <p className="word-split">To hide the slain? O, from this time forth,</p>
+    <p className="word-split">My thoughts be bloody or be nothing worth.</p>
+    <p className="stage-direction">[Exit.]</p>
+  </div>
+);
+
+export default Act4Scene4;
