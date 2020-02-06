848bdd5c65df05613f2933c3325b7e6b8caeb3c0
claracampos <camposclr@gmail.com>
2020-02-06 17:43:07 -0400

    added act 4, scene 1

---
+++ src/text/act4/Act4Scene1.js
@@ -0,0 +1,90 @@
+import React from "react";
+
+const Act4Scene1 = () => (
+  <div>
+    <h2>ACT IV</h2>
+    <h3>SCENE I. A room in the Castle.</h3>
+    <p className="stage-direction">
+      Enter King, Queen, Rosencrantz and Guildenstern.
+    </p>
+    <p className="character-name">KING.</p>
+    <p className="word-split">
+      There's matter in these sighs. These profound heaves
+    </p>
+    <p className="word-split">
+      You must translate. 'tis fit we understand them.
+    </p>
+    <p className="word-split">Where is your son?</p>
+    <p className="character-name">QUEEN.</p>
+    <p className="word-split">Bestow this place on us a little while.</p>
+    <p className="stage-direction">
+      [To Rosencrantz and Guildenstern, who go out.]
+    </p>
+    <p className="word-split">Ah, my good lord, what have I seen tonight!</p>
+    <p className="character-name">KING.</p>
+    <p className="word-split">What, Gertrude? How does Hamlet?</p>
+    <p className="character-name">QUEEN.</p>
+    <p className="word-split">Mad as the sea and wind, when both contend</p>
+    <p className="word-split">Which is the mightier. In his lawless fit</p>
+    <p className="word-split">Behind the arras hearing something stir,</p>
+    <p className="word-split">Whips out his rapier, cries "A rat, a rat!"</p>
+    <p className="word-split">And in this brainish apprehension kills</p>
+    <p className="word-split">The unseen good old man.</p>
+    <p className="character-name">KING.</p>
+    <p className="word-split">O heavy deed!</p>
+    <p className="word-split">It had been so with us, had we been there.</p>
+    <p className="word-split">His liberty is full of threats to all;</p>
+    <p className="word-split">To you yourself, to us, to everyone.</p>
+    <p className="word-split">Alas, how shall this bloody deed be answer'd?</p>
+    <p className="word-split">It will be laid to us, whose providence</p>
+    <p className="word-split">
+      Should have kept short, restrain'd, and out of haunt
+    </p>
+    <p className="word-split">This mad young man. But so much was our love</p>
+    <p className="word-split">We would not understand what was most fit,</p>
+    <p className="word-split">But like the owner of a foul disease,</p>
+    <p className="word-split">To keep it from divulging, let it feed</p>
+    <p className="word-split">Even on the pith of life. Where is he gone?</p>
+    <p className="character-name">QUEEN.</p>
+    <p className="word-split">To draw apart the body he hath kill'd,</p>
+    <p className="word-split">O'er whom his very madness, like some ore</p>
+    <p className="word-split">Among a mineral of metals base,</p>
+    <p className="word-split">Shows itself pure. He weeps for what is done.</p>
+    <p className="character-name">KING.</p>
+    <p className="word-split">O Gertrude, come away!</p>
+    <p className="word-split">The sun no sooner shall the mountains touch</p>
+    <p className="word-split">But we will ship him hence, and this vile deed</p>
+    <p className="word-split">We must with all our majesty and skill</p>
+    <p className="word-split">
+      Both countenance and excuse. -- Ho, Guildenstern!
+    </p>
+    <p className="stage-direction"> Re-enter Rosencrantz and Guildenstern.</p>
+    <p className="word-split">
+      Friends both, go join you with some further aid:
+    </p>
+    <p className="word-split">Hamlet in madness hath Polonius slain,</p>
+    <p className="word-split">
+      And from his mother's closet hath he dragg'd him.
+    </p>
+    <p className="word-split">
+      Go seek him out, speak fair, and bring the body
+    </p>
+    <p className="word-split">Into the chapel. I pray you haste in this.</p>
+    <p className="stage-direction">[Exeunt Rosencrantz and Guildenstern.]</p>
+    <p className="word-split">
+      Come, Gertrude, we'll call up our wisest friends,
+    </p>
+    <p className="word-split">And let them know both what we mean to do</p>
+    <p className="word-split">And what's untimely done, so haply slander,</p>
+    <p className="word-split">Whose whisper o'er the world's diameter,</p>
+    <p className="word-split">As level as the cannon to his blank,</p>
+    <p className="word-split">
+      Transports his poison'd shot, may miss our name,
+    </p>
+    <p className="word-split">And hit the woundless air. O, come away!</p>
+    <p className="word-split">My soul is full of discord and dismay.</p>
+    <p className="stage-direction">[Exeunt.]</p>
+  </div>
+);
+
+export default Act4Scene1;
