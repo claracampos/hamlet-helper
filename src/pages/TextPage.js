93e6b73aed0a8e097ca35b466bf8850db63b47a6
claracampos <camposclr@gmail.com>
2020-02-06 11:40:51 -0400

    added draft styling layout

--- src/pages/TextPage.js
+++ src/pages/TextPage.js
@@ -1,11 +1,14 @@
 import React from "react";
 
 const TextPage = props => (
-  <div>
-    <div>
+  <div className="d-flex flex-column vh-90">
+    <div className="main-text-container overflow-auto">
       <h2>{props.title}</h2>
       {props.scene}
     </div>
+    <footer className="footer mt-auto dictionary-container overflow-auto">
+      test
+    </footer>
   </div>
 );
 
