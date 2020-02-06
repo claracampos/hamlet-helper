d015b80acdef8c2e5265bcbf8590b6b227e8dbf4
claracampos <camposclr@gmail.com>
2020-02-06 17:54:43 -0400

    added links to pages

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -7,30 +7,34 @@ const Homepage = () => (
     <h2>Description here</h2>
     <Link>Dramatis Personae</Link>
     <p>ACT I</p>
-    <Link>Scene I. Elsinore. A platform before the Castle.</Link>
-    <Link>Scene II. Elsinore. A room of state in the Castle</Link>
-    <Link>Scene III. A room in Polonius’s house.</Link>
-    <Link>Scene IV. The platform.</Link>
-    <Link>Scene V. A more remote part of the Castle.</Link>
+    <Link to="/act1/scene1">
+      Scene I. Elsinore. A platform before the Castle.
+    </Link>
+    <Link to="/act1/scene2">
+      Scene II. Elsinore. A room of state in the Castle
+    </Link>
+    <Link to="/act1/scene3">Scene III. A room in Polonius’s house.</Link>
+    <Link to="/act1/scene4">Scene IV. The platform.</Link>
+    <Link to="/act1/scene5">Scene V. A more remote part of the Castle.</Link>
     <p>ACT II</p>
-    <Link>Scene I. A room in Polonius’s house.</Link>
-    <Link>Scene II. A room in the Castle.</Link>
+    <Link to="/act2/scene1">Scene I. A room in Polonius’s house.</Link>
+    <Link to="/act2/scene2">Scene II. A room in the Castle.</Link>
     <p>ACT III</p>
-    <Link>Scene I. A room in the Castle.</Link>
-    <Link>Scene II. A hall in the Castle.</Link>
-    <Link>Scene III. A room in the Castle.</Link>
-    <Link>Scene IV. Another room in the Castle.</Link>
+    <Link to="/act3/scene1">Scene I. A room in the Castle.</Link>
+    <Link to="/act3/scene2">Scene II. A hall in the Castle.</Link>
+    <Link to="/act3/scene3">Scene III. A room in the Castle.</Link>
+    <Link to="/act3/scene4">Scene IV. Another room in the Castle.</Link>
     <p>ACT IV</p>
-    <Link>Scene I. A room in the Castle.</Link>
-    <Link>Scene II. Another room in the Castle.</Link>
-    <Link>Scene III. Another room in the Castle.</Link>
-    <Link>Scene IV. A plain in Denmark.</Link>
-    <Link>Scene V. Elsinore. A room in the Castle.</Link>
-    <Link>Scene VI. Another room in the Castle.</Link>
-    <Link>Scene VII. Another room in the Castle.</Link>
+    <Link to="/act4/scene1">Scene I. A room in the Castle.</Link>
+    <Link to="/act4/scene2">Scene II. Another room in the Castle.</Link>
+    <Link to="/act4/scene3">Scene III. Another room in the Castle.</Link>
+    <Link to="/act4/scene4">Scene IV. A plain in Denmark.</Link>
+    <Link to="/act4/scene5">Scene V. Elsinore. A room in the Castle.</Link>
+    <Link to="/act4/scene6">Scene VI. Another room in the Castle.</Link>
+    <Link to="/act4/scene7">Scene VII. Another room in the Castle.</Link>
     <p>ACT V</p>
-    <Link>Scene I. A churchyard.</Link>
-    <Link>Scene II. A hall in the Castle.</Link>
+    <Link to="/act5/scene1">Scene I. A churchyard.</Link>
+    <Link to="/act5/scene2">Scene II. A hall in the Castle.</Link>
   </div>
 );
 
