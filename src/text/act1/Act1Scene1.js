26a5e3a7e3f9171b083383272376bb808a94b6e5
claracampos <camposclr@gmail.com>
2020-02-06 09:23:11 -0400

    added act 1, scene 1 text

---
+++ src/text/act1/Act1Scene1.js
@@ -0,0 +1,726 @@
+import React from "react";
+
+const Act1Scene1 = () => (
+  <div>
+    <h3>SCENE I. Elsinore. A platform before the Castle.</h3>
+
+    <p>
+      <i>Enter Francisco and Barnardo, two sentinels.</i>
+    </p>
+
+    <p>
+      BARNARDO.
+      <br />
+      Who&rsquo;s there?
+      <br />
+    </p>
+
+    <p>
+      FRANCISCO.
+      <br />
+      Nay, answer me. Stand and unfold yourself.
+      <br />
+    </p>
+
+    <p>
+      BARNARDO.
+      <br />
+      Long live the King!
+      <br />
+    </p>
+
+    <p>
+      FRANCISCO.
+      <br />
+      Barnardo?
+      <br />
+    </p>
+
+    <p>
+      BARNARDO.
+      <br />
+      He.
+      <br />
+    </p>
+
+    <p>
+      FRANCISCO.
+      <br />
+      You come most carefully upon your hour.
+      <br />
+    </p>
+
+    <p>
+      BARNARDO.
+      <br />
+      &rsquo;Tis now struck twelve. Get thee to bed, Francisco.
+      <br />
+    </p>
+
+    <p>
+      FRANCISCO.
+      <br />
+      For this relief much thanks. &rsquo;Tis bitter cold,
+      <br />
+      And I am sick at heart.
+      <br />
+    </p>
+
+    <p>
+      BARNARDO.
+      <br />
+      Have you had quiet guard?
+      <br />
+    </p>
+
+    <p>
+      FRANCISCO.
+      <br />
+      Not a mouse stirring.
+      <br />
+    </p>
+
+    <p>
+      BARNARDO.
+      <br />
+      Well, good night.
+      <br />
+      If you do meet Horatio and Marcellus,
+      <br />
+      The rivals of my watch, bid them make haste.
+      <br />
+    </p>
+
+    <p>
+      <i>Enter Horatio and Marcellus.</i>
+    </p>
+
+    <p>
+      FRANCISCO.
+      <br />
+      I think I hear them. Stand, ho! Who is there?
+      <br />
+    </p>
+
+    <p>
+      HORATIO.
+      <br />
+      Friends to this ground.
+      <br />
+    </p>
+
+    <p>
+      MARCELLUS.
+      <br />
+      And liegemen to the Dane.
+      <br />
+    </p>
+
+    <p>
+      FRANCISCO.
+      <br />
+      Give you good night.
+      <br />
+    </p>
+
+    <p>
+      MARCELLUS.
+      <br />
+      O, farewell, honest soldier, who hath reliev&rsquo;d you?
+      <br />
+    </p>
+
+    <p>
+      FRANCISCO.
+      <br />
+      Barnardo has my place. Give you good-night.
+      <br />
+    </p>
+
+    <p>
+      <i>Exit.</i>
+    </p>
+
+    <p>
+      MARCELLUS.
+      <br />
+      Holla, Barnardo!
+      <br />
+    </p>
+
+    <p>
+      BARNARDO.
+      <br />
+      Say, what, is Horatio there?
+      <br />
+    </p>
+
+    <p>
+      HORATIO.
+      <br />
+      A piece of him.
+      <br />
+    </p>
+
+    <p>
+      BARNARDO.
+      <br />
+      Welcome, Horatio. Welcome, good Marcellus.
+      <br />
+    </p>
+
+    <p>
+      MARCELLUS.
+      <br />
+      What, has this thing appear&rsquo;d again tonight?
+      <br />
+    </p>
+
+    <p>
+      BARNARDO.
+      <br />
+      I have seen nothing.
+      <br />
+    </p>
+
+    <p>
+      MARCELLUS.
+      <br />
+      Horatio says &rsquo;tis but our fantasy,
+      <br />
+      And will not let belief take hold of him
+      <br />
+      Touching this dreaded sight, twice seen of us.
+      <br />
+      Therefore I have entreated him along
+      <br />
+      With us to watch the minutes of this night,
+      <br />
+      That if again this apparition come
+      <br />
+      He may approve our eyes and speak to it.
+      <br />
+    </p>
+
+    <p>
+      HORATIO.
+      <br />
+      Tush, tush, &rsquo;twill not appear.
+      <br />
+    </p>
+
+    <p>
+      BARNARDO.
+      <br />
+      Sit down awhile,
+      <br />
+      And let us once again assail your ears,
+      <br />
+      That are so fortified against our story,
+      <br />
+      What we two nights have seen.
+      <br />
+    </p>
+
+    <p>
+      HORATIO.
+      <br />
+      Well, sit we down,
+      <br />
+      And let us hear Barnardo speak of this.
+      <br />
+    </p>
+
+    <p>
+      BARNARDO.
+      <br />
+      Last night of all,
+      <br />
+      When yond same star that&rsquo;s westward from the pole,
+      <br />
+      Had made his course t&rsquo;illume that part of heaven
+      <br />
+      Where now it burns, Marcellus and myself,
+      <br />
+      The bell then beating one&mdash;
+      <br />
+    </p>
+
+    <p>
+      MARCELLUS.
+      <br />
+      Peace, break thee off. Look where it comes again.
+      <br />
+    </p>
+
+    <p>
+      <i>Enter Ghost.</i>
+    </p>
+
+    <p>
+      BARNARDO.
+      <br />
+      In the same figure, like the King that&rsquo;s dead.
+      <br />
+    </p>
+
+    <p>
+      MARCELLUS.
+      <br />
+      Thou art a scholar; speak to it, Horatio.
+      <br />
+    </p>
+
+    <p>
+      BARNARDO.
+      <br />
+      Looks it not like the King? Mark it, Horatio.
+      <br />
+    </p>
+
+    <p>
+      HORATIO.
+      <br />
+      Most like. It harrows me with fear and wonder.
+      <br />
+    </p>
+
+    <p>
+      BARNARDO
+      <br />
+      It would be spoke to.
+      <br />
+    </p>
+
+    <p>
+      MARCELLUS.
+      <br />
+      Question it, Horatio.
+      <br />
+    </p>
+
+    <p>
+      HORATIO.
+      <br />
+      What art thou that usurp&rsquo;st this time of night,
+      <br />
+      Together with that fair and warlike form
+      <br />
+      In which the majesty of buried Denmark
+      <br />
+      Did sometimes march? By heaven I charge thee speak.
+      <br />
+    </p>
+
+    <p>
+      MARCELLUS.
+      <br />
+      It is offended.
+      <br />
+    </p>
+
+    <p>
+      BARNARDO.
+      <br />
+      See, it stalks away.
+      <br />
+    </p>
+
+    <p>
+      HORATIO.
+      <br />
+      Stay! speak, speak! I charge thee speak!
+      <br />
+    </p>
+
+    <p>
+      <i>Exit Ghost.</i>
+    </p>
+
+    <p>
+      MARCELLUS.
+      <br />
+      &rsquo;Tis gone, and will not answer.
+      <br />
+    </p>
+
+    <p>
+      BARNARDO.
+      <br />
+      How now, Horatio! You tremble and look pale.
+      <br />
+      Is not this something more than fantasy?
+      <br />
+      What think you on&rsquo;t?
+      <br />
+    </p>
+
+    <p>
+      HORATIO.
+      <br />
+      Before my God, I might not this believe
+      <br />
+      Without the sensible and true avouch
+      <br />
+      Of mine own eyes.
+      <br />
+    </p>
+
+    <p>
+      MARCELLUS.
+      <br />
+      Is it not like the King?
+      <br />
+    </p>
+
+    <p>
+      HORATIO.
+      <br />
+      As thou art to thyself:
+      <br />
+      Such was the very armour he had on
+      <br />
+      When he th&rsquo;ambitious Norway combated;
+      <br />
+      So frown&rsquo;d he once, when in an angry parle
+      <br />
+      He smote the sledded Polacks on the ice.
+      <br />
+      &rsquo;Tis strange.
+      <br />
+    </p>
+
+    <p>
+      MARCELLUS.
+      <br />
+      Thus twice before, and jump at this dead hour,
+      <br />
+      With martial stalk hath he gone by our watch.
+      <br />
+    </p>
+
+    <p>
+      HORATIO.
+      <br />
+      In what particular thought to work I know not;
+      <br />
+      But in the gross and scope of my opinion,
+      <br />
+      This bodes some strange eruption to our state.
+      <br />
+    </p>
+
+    <p>
+      MARCELLUS.
+      <br />
+      Good now, sit down, and tell me, he that knows,
+      <br />
+      Why this same strict and most observant watch
+      <br />
+      So nightly toils the subject of the land,
+      <br />
+      And why such daily cast of brazen cannon
+      <br />
+      And foreign mart for implements of war;
+      <br />
+      Why such impress of shipwrights, whose sore task
+      <br />
+      Does not divide the Sunday from the week.
+      <br />
+      What might be toward, that this sweaty haste
+      <br />
+      Doth make the night joint-labourer with the day:
+      <br />
+      Who is&rsquo;t that can inform me?
+      <br />
+    </p>
+
+    <p>
+      HORATIO.
+      <br />
+      That can I;
+      <br />
+      At least, the whisper goes so. Our last King,
+      <br />
+      Whose image even but now appear&rsquo;d to us,
+      <br />
+      Was, as you know, by Fortinbras of Norway,
+      <br />
+      Thereto prick&rsquo;d on by a most emulate pride,
+      <br />
+      Dar&rsquo;d to the combat; in which our valiant Hamlet,
+      <br />
+      For so this side of our known world esteem&rsquo;d him,
+      <br />
+      Did slay this Fortinbras; who by a seal&rsquo;d compact,
+      <br />
+      Well ratified by law and heraldry,
+      <br />
+      Did forfeit, with his life, all those his lands
+      <br />
+      Which he stood seiz&rsquo;d of, to the conqueror;
+      <br />
+      Against the which, a moiety competent
+      <br />
+      Was gaged by our King; which had return&rsquo;d
+      <br />
+      To the inheritance of Fortinbras,
+      <br />
+      Had he been vanquisher; as by the same cov&rsquo;nant
+      <br />
+      And carriage of the article design&rsquo;d,
+      <br />
+      His fell to Hamlet. Now, sir, young Fortinbras,
+      <br />
+      Of unimproved mettle, hot and full,
+      <br />
+      Hath in the skirts of Norway, here and there,
+      <br />
+      Shark&rsquo;d up a list of lawless resolutes,
+      <br />
+      For food and diet, to some enterprise
+      <br />
+      That hath a stomach in&rsquo;t; which is no other,
+      <br />
+      As it doth well appear unto our state,
+      <br />
+      But to recover of us by strong hand
+      <br />
+      And terms compulsatory, those foresaid lands
+      <br />
+      So by his father lost. And this, I take it,
+      <br />
+      Is the main motive of our preparations,
+      <br />
+      The source of this our watch, and the chief head
+      <br />
+      Of this post-haste and rummage in the land.
+      <br />
+    </p>
+
+    <p>
+      BARNARDO.
+      <br />
+      I think it be no other but e&rsquo;en so:
+      <br />
+      Well may it sort that this portentous figure
+      <br />
+      Comes armed through our watch so like the King
+      <br />
+      That was and is the question of these wars.
+      <br />
+    </p>
+
+    <p>
+      HORATIO.
+      <br />
+      A mote it is to trouble the mind&rsquo;s eye.
+      <br />
+      In the most high and palmy state of Rome,
+      <br />
+      A little ere the mightiest Julius fell,
+      <br />
+      The graves stood tenantless and the sheeted dead
+      <br />
+      Did squeak and gibber in the Roman streets;
+      <br />
+      As stars with trains of fire and dews of blood,
+      <br />
+      Disasters in the sun; and the moist star,
+      <br />
+      Upon whose influence Neptune&rsquo;s empire stands,
+      <br />
+      Was sick almost to doomsday with eclipse.
+      <br />
+      And even the like precurse of fierce events,
+      <br />
+      As harbingers preceding still the fates
+      <br />
+      And prologue to the omen coming on,
+      <br />
+      Have heaven and earth together demonstrated
+      <br />
+      Unto our climatures and countrymen.
+      <br />
+    </p>
+
+    <p>
+      <i>Re-enter Ghost.</i>
+    </p>
+
+    <p>
+      But, soft, behold! Lo, where it comes again!
+      <br />
+      I&rsquo;ll cross it, though it blast me. Stay, illusion!
+      <br />
+      If thou hast any sound, or use of voice,
+      <br />
+      Speak to me.
+      <br />
+      If there be any good thing to be done,
+      <br />
+      That may to thee do ease, and grace to me,
+      <br />
+      Speak to me.
+      <br />
+      If thou art privy to thy country&rsquo;s fate,
+      <br />
+      Which, happily, foreknowing may avoid,
+      <br />
+      O speak!
+      <br />
+      Or if thou hast uphoarded in thy life
+      <br />
+      Extorted treasure in the womb of earth,
+      <br />
+      For which, they say, you spirits oft walk in death,
+      <br />
+      Speak of it. Stay, and speak!
+    </p>
+
+    <p>
+      <i>The cock crows.</i>
+    </p>
+
+    <p>
+      Stop it, Marcellus!
+      <br />
+    </p>
+
+    <p>
+      MARCELLUS.
+      <br />
+      Shall I strike at it with my partisan?
+      <br />
+    </p>
+
+    <p>
+      HORATIO.
+      <br />
+      Do, if it will not stand.
+      <br />
+    </p>
+
+    <p>
+      BARNARDO.
+      <br />
+      &rsquo;Tis here!
+      <br />
+    </p>
+
+    <p>
+      HORATIO.
+      <br />
+      &rsquo;Tis here!
+      <br />
+    </p>
+
+    <p>
+      <i>Exit Ghost.</i>
+    </p>
+
+    <p>
+      MARCELLUS.
+      <br />
+      &rsquo;Tis gone!
+      <br />
+      We do it wrong, being so majestical,
+      <br />
+      To offer it the show of violence,
+      <br />
+      For it is as the air, invulnerable,
+      <br />
+      And our vain blows malicious mockery.
+      <br />
+    </p>
+
+    <p>
+      BARNARDO.
+      <br />
+      It was about to speak, when the cock crew.
+      <br />
+    </p>
+
+    <p>
+      HORATIO.
+      <br />
+      And then it started, like a guilty thing
+      <br />
+      Upon a fearful summons. I have heard
+      <br />
+      The cock, that is the trumpet to the morn,
+      <br />
+      Doth with his lofty and shrill-sounding throat
+      <br />
+      Awake the god of day; and at his warning,
+      <br />
+      Whether in sea or fire, in earth or air,
+      <br />
+      Th&rsquo;extravagant and erring spirit hies
+      <br />
+      To his confine. And of the truth herein
+      <br />
+      This present object made probation.
+      <br />
+    </p>
+
+    <p>
+      MARCELLUS.
+      <br />
+      It faded on the crowing of the cock.
+      <br />
+      Some say that ever &rsquo;gainst that season comes
+      <br />
+      Wherein our Saviour&rsquo;s birth is celebrated,
+      <br />
+      The bird of dawning singeth all night long;
+      <br />
+      And then, they say, no spirit dare stir abroad,
+      <br />
+      The nights are wholesome, then no planets strike,
+      <br />
+      No fairy takes, nor witch hath power to charm;
+      <br />
+      So hallow&rsquo;d and so gracious is the time.
+      <br />
+    </p>
+
+    <p>
+      HORATIO.
+      <br />
+      So have I heard, and do in part believe it.
+      <br />
+      But look, the morn in russet mantle clad,
+      <br />
+      Walks o&rsquo;er the dew of yon high eastward hill.
+      <br />
+      Break we our watch up, and by my advice,
+      <br />
+      Let us impart what we have seen tonight
+      <br />
+      Unto young Hamlet; for upon my life,
+      <br />
+      This spirit, dumb to us, will speak to him.
+      <br />
+      Do you consent we shall acquaint him with it,
+      <br />
+      As needful in our loves, fitting our duty?
+      <br />
+    </p>
+
+    <p>
+      MARCELLUS.
+      <br />
+      Let&rsquo;s do&rsquo;t, I pray, and I this morning know
+      <br />
+      Where we shall find him most conveniently.
+      <br />
+    </p>
+
+    <p>
+      <i>Exeunt.</i>
+    </p>
+  </div>
+);
+
+export default Act1Scene1;
