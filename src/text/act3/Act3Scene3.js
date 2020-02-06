24ab3cd807ec76e4045474a4773e53b1e4c74b77
claracampos <camposclr@gmail.com>
2020-02-06 17:42:52 -0400

    added act 3, scene 3

---
+++ src/text/act3/Act3Scene3.js
@@ -0,0 +1,159 @@
+import React from "react";
+
+const Act3Scene3 = () => (
+  <div>
+    {" "}
+    <h2>ACT III</h2>
+    <h3>SCENE III. A room in the Castle.</h3>
+    <p className="stage-direction">
+      {" "}
+      Enter King, Rosencrantz and Guildenstern.
+    </p>
+    <p className="character-name">KING.</p>
+    <p className="word-split">I like him not, nor stands it safe with us</p>
+    <p className="word-split">
+      To let his madness range. Therefore prepare you,
+    </p>
+    <p className="word-split">I your commission will forthwith dispatch,</p>
+    <p className="word-split">And he to England shall along with you.</p>
+    <p className="word-split">The terms of our estate may not endure</p>
+    <p className="word-split">Hazard so near us as doth hourly grow</p>
+    <p className="word-split">Out of his lunacies.</p>
+    <p className="character-name">GUILDENSTERN.</p>
+    <p className="word-split">We will ourselves provide.</p>
+    <p className="word-split">Most holy and religious fear it is</p>
+    <p className="word-split">To keep those many many bodies safe</p>
+    <p className="word-split">That live and feed upon your Majesty.</p>
+    <p className="character-name">ROSENCRANTZ.</p>
+    <p className="word-split">The single and peculiar life is bound</p>
+    <p className="word-split">With all the strength and armour of the mind,</p>
+    <p className="word-split">To keep itself from 'noyance; but much more</p>
+    <p className="word-split">That spirit upon whose weal depend and rest</p>
+    <p className="word-split">The lives of many. The cease of majesty</p>
+    <p className="word-split">Dies not alone; but like a gulf doth draw</p>
+    <p className="word-split">What's near it with it. It is a massy wheel</p>
+    <p className="word-split">Fix'd on the summit of the highest mount,</p>
+    <p className="word-split">
+      To whose huge spokes ten thousand lesser things
+    </p>
+    <p className="word-split">
+      Are mortis'd and adjoin'd; which when it falls,
+    </p>
+    <p className="word-split">Each small annexment, petty consequence,</p>
+    <p className="word-split">Attends the boist'rous ruin. Never alone</p>
+    <p className="word-split">Did the King sigh, but with a general groan.</p>
+    <p className="character-name">KING.</p>
+    <p className="word-split">Arm you, I pray you, to this speedy voyage;</p>
+    <p className="word-split">For we will fetters put upon this fear,</p>
+    <p className="word-split">Which now goes too free-footed.</p>
+    <p className="character-name">ROSENCRANTZ and GUILDENSTERN.</p>
+    <p className="word-split">We will haste us.</p>
+    <p className="stage-direction">[Exeunt Rosencrantz and Guildenstern.]</p>
+    <p className="stage-direction"> Enter Polonius.</p>
+    <p className="character-name">POLONIUS.</p>
+    <p className="word-split">My lord, he's going to his mother's closet.</p>
+    <p className="word-split">Behind the arras I'll convey myself</p>
+    <p className="word-split">
+      To hear the process. I'll warrant she'll tax him home,
+    </p>
+    <p className="word-split">And as you said, and wisely was it said,</p>
+    <p className="word-split">
+      'Tis meet that some more audience than a mother,
+    </p>
+    <p className="word-split">
+      Since nature makes them partial, should o'erhear
+    </p>
+    <p className="word-split">
+      The speech of vantage. Fare you well, my liege,
+    </p>
+    <p className="word-split">I'll call upon you ere you go to bed,</p>
+    <p className="word-split">And tell you what I know.</p>
+    <p className="character-name">KING.</p>
+    <p className="word-split">Thanks, dear my lord.</p>
+    <p className="stage-direction">[Exit Polonius.]</p>
+    <p className="word-split">O, my offence is rank, it smells to heaven;</p>
+    <p className="word-split">It hath the primal eldest curse upon't, -- </p>
+    <p className="word-split">A brother's murder! Pray can I not,</p>
+    <p className="word-split">Though inclination be as sharp as will:</p>
+    <p className="word-split">My stronger guilt defeats my strong intent,</p>
+    <p className="word-split">And, like a man to double business bound,</p>
+    <p className="word-split">I stand in pause where I shall first begin,</p>
+    <p className="word-split">And both neglect. What if this cursed hand</p>
+    <p className="word-split">Were thicker than itself with brother's blood,</p>
+    <p className="word-split">Is there not rain enough in the sweet heavens</p>
+    <p className="word-split">To wash it white as snow? Whereto serves mercy</p>
+    <p className="word-split">But to confront the visage of offence?</p>
+    <p className="word-split">And what's in prayer but this twofold force,</p>
+    <p className="word-split">To be forestalled ere we come to fall,</p>
+    <p className="word-split">Or pardon'd being down? Then I'll look up.</p>
+    <p className="word-split">My fault is past. But O, what form of prayer</p>
+    <p className="word-split">Can serve my turn? Forgive me my foul murder!</p>
+    <p className="word-split">That cannot be; since I am still possess'd</p>
+    <p className="word-split">
+      Of those effects for which I did the murder, --{" "}
+    </p>
+    <p className="word-split">My crown, mine own ambition, and my queen.</p>
+    <p className="word-split">May one be pardon'd and retain th'offence?</p>
+    <p className="word-split">In the corrupted currents of this world</p>
+    <p className="word-split">Offence's gilded hand may shove by justice,</p>
+    <p className="word-split">And oft 'tis seen the wicked prize itself</p>
+    <p className="word-split">Buys out the law. But 'tis not so above;</p>
+    <p className="word-split">There is no shuffling, there the action lies</p>
+    <p className="word-split">In his true nature, and we ourselves compell'd</p>
+    <p className="word-split">Even to the teeth and forehead of our faults,</p>
+    <p className="word-split">To give in evidence. What then? What rests?</p>
+    <p className="word-split">Try what repentance can. What can it not?</p>
+    <p className="word-split">Yet what can it, when one cannot repent?</p>
+    <p className="word-split">O wretched state! O bosom black as death!</p>
+    <p className="word-split">O limed soul, that struggling to be free,</p>
+    <p className="word-split">Art more engag'd! Help, angels! Make assay:</p>
+    <p className="word-split">
+      Bow, stubborn knees; and heart with strings of steel,
+    </p>
+    <p className="word-split">Be soft as sinews of the new-born babe.</p>
+    <p className="word-split">All may be well.</p>
+    <p className="stage-direction">[Retires and kneels.]</p>
+    <p className="stage-direction"> Enter Hamlet.</p>
+    <p className="character-name">HAMLET.</p>
+    <p className="word-split">Now might I do it pat, now he is praying.</p>
+    <p className="word-split">And now I'll do't. And so he goes to heaven;</p>
+    <p className="word-split">And so am I reveng'd. That would be scann'd:</p>
+    <p className="word-split">A villain kills my father, and for that</p>
+    <p className="word-split">I, his sole son, do this same villain send</p>
+    <p className="word-split">
+      To heaven. O, this is hire and salary, not revenge.
+    </p>
+    <p className="word-split">He took my father grossly, full of bread,</p>
+    <p className="word-split">
+      With all his crimes broad blown, as flush as May;
+    </p>
+    <p className="word-split">
+      And how his audit stands, who knows save heaven?
+    </p>
+    <p className="word-split">But in our circumstance and course of thought,</p>
+    <p className="word-split">'Tis heavy with him. And am I then reveng'd,</p>
+    <p className="word-split">To take him in the purging of his soul,</p>
+    <p className="word-split">
+      When he is fit and season'd for his passage? No.
+    </p>
+    <p className="word-split">Up, sword, and know thou a more horrid hent:</p>
+    <p className="word-split">When he is drunk asleep; or in his rage,</p>
+    <p className="word-split">Or in th'incestuous pleasure of his bed,</p>
+    <p className="word-split">At gaming, swearing; or about some act</p>
+    <p className="word-split">That has no relish of salvation in't,</p>
+    <p className="word-split">
+      Then trip him, that his heels may kick at heaven,
+    </p>
+    <p className="word-split">And that his soul may be as damn'd and black</p>
+    <p className="word-split">As hell, whereto it goes. My mother stays.</p>
+    <p className="word-split">This physic but prolongs thy sickly days.</p>
+    <p className="stage-direction">[Exit.]</p>
+    <p className="stage-direction">[The King rises and advances.]</p>
+    <p className="character-name">KING.</p>
+    <p className="word-split">My words fly up, my thoughts remain below.</p>
+    <p className="word-split">Words without thoughts never to heaven go.</p>
+    <p className="stage-direction">[Exit.]</p>
+  </div>
+);
+
+export default Act3Scene3;
