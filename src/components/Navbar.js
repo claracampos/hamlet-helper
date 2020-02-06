b9ce27739cac5abf57922927a1e5d7bf80b470d3
claracampos <camposclr@gmail.com>
2020-02-06 12:48:43 -0400

    added bootstrap navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,59 @@
 import React from "react";
 
 const Navbar = () => (
-  <div>
-    <button>about</button>
-  </div>
+  <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <a className="navbar-brand" href="#">
+      Hamlet Helper
+    </a>
+    <button
+      className="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span className="navbar-toggler-icon"></span>
+    </button>
+
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav mr-auto">
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            About
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            Help
+          </a>
+        </li>
+        <li className="nav-item dropdown">
+          <a
+            className="nav-link dropdown-toggle"
+            href="#"
+            id="navbarDropdown"
+            role="button"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            Read
+          </a>
+          <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a className="dropdown-item" href="#">
+              Act I
+            </a>
+            <div className="dropdown-divider"></div>
+            <a className="dropdown-item" href="#">
+              Act II
+            </a>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </nav>
 );
 
 export default Navbar;
