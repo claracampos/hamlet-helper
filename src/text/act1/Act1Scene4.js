04d8f60d40d0abdf2c375c38fef267f34d214d2f
claracampos <camposclr@gmail.com>
2020-02-06 15:14:20 -0400

    formatted text for rendering

--- src/text/act1/Act1Scene4.js
+++ src/text/act1/Act1Scene4.js
@@ -2,391 +2,220 @@ import React from "react";
 
 const Act1Scene4 = () => (
   <div>
-    <h4>
-      <b>SCENE IV. The platform.</b>
-    </h4>
+    <h2>ACT I</h2>
+    <h3>SCENE IV. The platform.</h3>
 
-    <p>
-      <i>Enter Hamlet, Horatio and Marcellus.</i>
-    </p>
+    <p className="stage-direction">Enter Hamlet, Horatio and Marcellus.</p>
 
-    <p>
-      HAMLET.
-      <br />
-      The air bites shrewdly; it is very cold.
-      <br />
-    </p>
+    <p className="character-name">HAMLET.</p>
+    <p className="word-split">The air bites shrewdly; it is very cold.</p>
 
-    <p>
-      HORATIO.
-      <br />
-      It is a nipping and an eager air.
-      <br />
-    </p>
+    <p className="character-name">HORATIO.</p>
+    <p className="word-split">It is a nipping and an eager air.</p>
 
-    <p>
-      HAMLET.
-      <br />
-      What hour now?
-      <br />
-    </p>
+    <p className="character-name">HAMLET.</p>
+    <p className="word-split">What hour now?</p>
 
-    <p>
-      HORATIO.
-      <br />
-      I think it lacks of twelve.
-      <br />
-    </p>
+    <p className="character-name">HORATIO.</p>
+    <p className="word-split">I think it lacks of twelve.</p>
 
-    <p>
-      MARCELLUS.
-      <br />
-      No, it is struck.
-      <br />
-    </p>
+    <p className="character-name">MARCELLUS.</p>
+    <p className="word-split">No, it is struck.</p>
 
-    <p>
-      HORATIO.
-      <br />
+    <p className="character-name">HORATIO.</p>
+    <p className="word-split">
       Indeed? I heard it not. It then draws near the season
-      <br />
-      Wherein the spirit held his wont to walk.
-      <br />
+    </p>
+    <p className="word-split">Wherein the spirit held his wont to walk.</p>
+
+    <p className="stage-direction">
+      [A flourish of trumpets, and ordnance shot off within.]
     </p>
 
-    <p>
-      <i>[A flourish of trumpets, and ordnance shot off within.]</i>
-    </p>
+    <p className="word-split">What does this mean, my lord?</p>
 
-    <p>What does this mean, my lord?</p>
-
-    <p>
-      HAMLET.
-      <br />
+    <p className="character-name">HAMLET.</p>
+    <p className="word-split">
       The King doth wake tonight and takes his rouse,
-      <br />
+    </p>
+    <p className="word-split">
       Keeps wassail, and the swaggering upspring reels;
-      <br />
-      And as he drains his draughts of Rhenish down,
-      <br />
-      The kettle-drum and trumpet thus bray out
-      <br />
-      The triumph of his pledge.
-      <br />
     </p>
+    <p className="word-split">And as he drains his draughts of Rhenish down,</p>
+    <p className="word-split">The kettle-drum and trumpet thus bray out</p>
+    <p className="word-split">The triumph of his pledge.</p>
 
-    <p>
-      HORATIO.
-      <br />
-      Is it a custom?
-      <br />
+    <p className="character-name">HORATIO.</p>
+    <p className="word-split">Is it a custom?</p>
+
+    <p className="character-name">HAMLET.</p>
+    <p className="word-split">Ay marry is't;</p>
+    <p className="word-split">And to my mind, though I am native here,</p>
+    <p className="word-split">And to the manner born, it is a custom</p>
+    <p className="word-split">
+      More honour'd in the breach than the observance.
     </p>
-
-    <p>
-      HAMLET.
-      <br />
-      Ay marry is&rsquo;t;
-      <br />
-      And to my mind, though I am native here,
-      <br />
-      And to the manner born, it is a custom
-      <br />
-      More honour&rsquo;d in the breach than the observance.
-      <br />
-      This heavy-headed revel east and west
-      <br />
-      Makes us traduc&rsquo;d and tax&rsquo;d of other nations:
-      <br />
+    <p className="word-split">This heavy-headed revel east and west</p>
+    <p className="word-split">Makes us traduc'd and tax'd of other nations:</p>
+    <p className="word-split">
       They clepe us drunkards, and with swinish phrase
-      <br />
-      Soil our addition; and indeed it takes
-      <br />
-      From our achievements, though perform&rsquo;d at height,
-      <br />
-      The pith and marrow of our attribute.
-      <br />
-      So oft it chances in particular men
-      <br />
-      That for some vicious mole of nature in them,
-      <br />
+    </p>
+    <p className="word-split">Soil our addition; and indeed it takes</p>
+    <p className="word-split">
+      From our achievements, though perform'd at height,
+    </p>
+    <p className="word-split">The pith and marrow of our attribute.</p>
+    <p className="word-split">So oft it chances in particular men</p>
+    <p className="word-split">That for some vicious mole of nature in them,</p>
+    <p className="word-split">
       As in their birth, wherein they are not guilty,
-      <br />
-      Since nature cannot choose his origin,
-      <br />
-      By their o&rsquo;ergrowth of some complexion,
-      <br />
+    </p>
+    <p className="word-split">Since nature cannot choose his origin,</p>
+    <p className="word-split">By their o'ergrowth of some complexion,</p>
+    <p className="word-split">
       Oft breaking down the pales and forts of reason;
-      <br />
-      Or by some habit, that too much o&rsquo;erleavens
-      <br />
-      The form of plausive manners;&mdash;that these men,
-      <br />
-      Carrying, I say, the stamp of one defect,
-      <br />
-      Being Nature&rsquo;s livery or Fortune&rsquo;s star,&mdash;
-      <br />
-      His virtues else,&mdash;be they as pure as grace,
-      <br />
-      As infinite as man may undergo,
-      <br />
-      Shall in the general censure take corruption
-      <br />
-      From that particular fault. The dram of evil
-      <br />
-      Doth all the noble substance often doubt
-      <br />
-      To his own scandal.
-      <br />
     </p>
-
-    <p>
-      HORATIO.
-      <br />
-      Look, my lord, it comes!
-      <br />
+    <p className="word-split">Or by some habit, that too much o'erleavens</p>
+    <p className="word-split">
+      The form of plausive manners; -- that these men,
     </p>
+    <p className="word-split">Carrying, I say, the stamp of one defect,</p>
+    <p className="word-split">Being Nature's livery or Fortune's star, -- </p>
+    <p className="word-split">His virtues else, -- be they as pure as grace,</p>
+    <p className="word-split">As infinite as man may undergo,</p>
+    <p className="word-split">Shall in the general censure take corruption</p>
+    <p className="word-split">From that particular fault. The dram of evil</p>
+    <p className="word-split">Doth all the noble substance often doubt</p>
+    <p className="word-split">To his own scandal.</p>
 
-    <p>
-      <i>Enter Ghost.</i>
-    </p>
+    <p className="character-name">HORATIO.</p>
+    <p className="word-split">Look, my lord, it comes!</p>
 
-    <p>
-      HAMLET.
-      <br />
-      Angels and ministers of grace defend us!
-      <br />
-      Be thou a spirit of health or goblin damn&rsquo;d,
-      <br />
+    <p className="stage-direction">Enter Ghost.</p>
+
+    <p className="character-name">HAMLET.</p>
+    <p className="word-split">Angels and ministers of grace defend us!</p>
+    <p className="word-split">Be thou a spirit of health or goblin damn'd,</p>
+    <p className="word-split">
       Bring with thee airs from heaven or blasts from hell,
-      <br />
-      Be thy intents wicked or charitable,
-      <br />
-      Thou com&rsquo;st in such a questionable shape
-      <br />
-      That I will speak to thee. I&rsquo;ll call thee Hamlet,
-      <br />
-      King, father, royal Dane. O, answer me!
-      <br />
-      Let me not burst in ignorance; but tell
-      <br />
-      Why thy canoniz&rsquo;d bones, hearsed in death,
-      <br />
-      Have burst their cerements; why the sepulchre,
-      <br />
-      Wherein we saw thee quietly inurn&rsquo;d,
-      <br />
-      Hath op&rsquo;d his ponderous and marble jaws
-      <br />
-      To cast thee up again! What may this mean,
-      <br />
+    </p>
+    <p className="word-split">Be thy intents wicked or charitable,</p>
+    <p className="word-split">Thou com'st in such a questionable shape</p>
+    <p className="word-split">
+      That I will speak to thee. I'll call thee Hamlet,
+    </p>
+    <p className="word-split">King, father, royal Dane. O, answer me!</p>
+    <p className="word-split">Let me not burst in ignorance; but tell</p>
+    <p className="word-split">Why thy canoniz'd bones, hearsed in death,</p>
+    <p className="word-split">Have burst their cerements; why the sepulchre,</p>
+    <p className="word-split">Wherein we saw thee quietly inurn'd,</p>
+    <p className="word-split">Hath op'd his ponderous and marble jaws</p>
+    <p className="word-split">To cast thee up again! What may this mean,</p>
+    <p className="word-split">
       That thou, dead corse, again in complete steel,
-      <br />
-      Revisit&rsquo;st thus the glimpses of the moon,
-      <br />
-      Making night hideous, and we fools of nature
-      <br />
-      So horridly to shake our disposition
-      <br />
-      With thoughts beyond the reaches of our souls?
-      <br />
+    </p>
+    <p className="word-split">Revisit'st thus the glimpses of the moon,</p>
+    <p className="word-split">Making night hideous, and we fools of nature</p>
+    <p className="word-split">So horridly to shake our disposition</p>
+    <p className="word-split">With thoughts beyond the reaches of our souls?</p>
+    <p className="word-split">
       Say, why is this? Wherefore? What should we do?
-      <br />
     </p>
 
-    <p>
-      <i>Ghost beckons Hamlet.</i>
-    </p>
+    <p className="stage-direction">Ghost beckons Hamlet.</p>
 
-    <p>
-      HORATIO.
-      <br />
-      It beckons you to go away with it,
-      <br />
-      As if it some impartment did desire
-      <br />
-      To you alone.
-      <br />
-    </p>
+    <p className="character-name">HORATIO.</p>
+    <p className="word-split">It beckons you to go away with it,</p>
+    <p className="word-split">As if it some impartment did desire</p>
+    <p className="word-split">To you alone.</p>
 
-    <p>
-      MARCELLUS.
-      <br />
-      Look with what courteous action
-      <br />
-      It waves you to a more removed ground.
-      <br />
-      But do not go with it.
-      <br />
-    </p>
+    <p className="character-name">MARCELLUS.</p>
+    <p className="word-split">Look with what courteous action</p>
+    <p className="word-split">It waves you to a more removed ground.</p>
+    <p className="word-split">But do not go with it.</p>
 
-    <p>
-      HORATIO.
-      <br />
-      No, by no means.
-      <br />
-    </p>
+    <p className="character-name">HORATIO.</p>
+    <p className="word-split">No, by no means.</p>
 
-    <p>
-      HAMLET.
-      <br />
-      It will not speak; then will I follow it.
-      <br />
-    </p>
+    <p className="character-name">HAMLET.</p>
+    <p className="word-split">It will not speak; then will I follow it.</p>
 
-    <p>
-      HORATIO.
-      <br />
-      Do not, my lord.
-      <br />
-    </p>
+    <p className="character-name">HORATIO.</p>
+    <p className="word-split">Do not, my lord.</p>
 
-    <p>
-      HAMLET.
-      <br />
-      Why, what should be the fear?
-      <br />
-      I do not set my life at a pin&rsquo;s fee;
-      <br />
-      And for my soul, what can it do to that,
-      <br />
-      Being a thing immortal as itself?
-      <br />
-      It waves me forth again. I&rsquo;ll follow it.
-      <br />
-    </p>
+    <p className="character-name">HAMLET.</p>
+    <p className="word-split">Why, what should be the fear?</p>
+    <p className="word-split">I do not set my life at a pin's fee;</p>
+    <p className="word-split">And for my soul, what can it do to that,</p>
+    <p className="word-split">Being a thing immortal as itself?</p>
+    <p className="word-split">It waves me forth again. I'll follow it.</p>
 
-    <p>
-      HORATIO.
-      <br />
+    <p className="character-name">HORATIO.</p>
+    <p className="word-split">
       What if it tempt you toward the flood, my lord,
-      <br />
-      Or to the dreadful summit of the cliff
-      <br />
-      That beetles o&rsquo;er his base into the sea,
-      <br />
-      And there assume some other horrible form
-      <br />
+    </p>
+    <p className="word-split">Or to the dreadful summit of the cliff</p>
+    <p className="word-split">That beetles o'er his base into the sea,</p>
+    <p className="word-split">And there assume some other horrible form</p>
+    <p className="word-split">
       Which might deprive your sovereignty of reason,
-      <br />
-      And draw you into madness? Think of it.
-      <br />
-      The very place puts toys of desperation,
-      <br />
-      Without more motive, into every brain
-      <br />
-      That looks so many fadoms to the sea
-      <br />
-      And hears it roar beneath.
-      <br />
     </p>
+    <p className="word-split">And draw you into madness? Think of it.</p>
+    <p className="word-split">The very place puts toys of desperation,</p>
+    <p className="word-split">Without more motive, into every brain</p>
+    <p className="word-split">That looks so many fadoms to the sea</p>
+    <p className="word-split">And hears it roar beneath.</p>
 
-    <p>
-      HAMLET.
-      <br />
-      It waves me still.
-      <br />
-      Go on, I&rsquo;ll follow thee.
-      <br />
+    <p className="character-name">HAMLET.</p>
+    <p className="word-split">It waves me still.</p>
+    <p className="word-split">Go on, I'll follow thee.</p>
+
+    <p className="character-name">MARCELLUS.</p>
+    <p className="word-split">You shall not go, my lord.</p>
+
+    <p className="character-name">HAMLET.</p>
+    <p className="word-split">Hold off your hands.</p>
+
+    <p className="character-name">HORATIO.</p>
+    <p className="word-split">Be rul'd; you shall not go.</p>
+
+    <p className="character-name">HAMLET.</p>
+    <p className="word-split">My fate cries out,</p>
+    <p className="word-split">And makes each petty artery in this body</p>
+    <p className="word-split">As hardy as the Nemean lion's nerve.</p>
+
+    <p className="stage-direction">[Ghost beckons.]</p>
+
+    <p className="word-split">Still am I call'd. Unhand me, gentlemen.</p>
+
+    <p className="stage-direction">[Breaking free from them.]</p>
+
+    <p className="word-split">
+      By heaven, I'll make a ghost of him that lets me.
     </p>
+    <p className="word-split">I say, away! -- Go on, I'll follow thee.</p>
 
-    <p>
-      MARCELLUS.
-      <br />
-      You shall not go, my lord.
-      <br />
-    </p>
+    <p className="stage-direction">Exeunt Ghost and Hamlet.</p>
 
-    <p>
-      HAMLET.
-      <br />
-      Hold off your hands.
-      <br />
-    </p>
+    <p className="character-name">HORATIO.</p>
+    <p className="word-split">He waxes desperate with imagination.</p>
 
-    <p>
-      HORATIO.
-      <br />
-      Be rul&rsquo;d; you shall not go.
-      <br />
-    </p>
+    <p className="character-name">MARCELLUS.</p>
+    <p className="word-split">Let's follow; 'tis not fit thus to obey him.</p>
 
-    <p>
-      HAMLET.
-      <br />
-      My fate cries out,
-      <br />
-      And makes each petty artery in this body
-      <br />
-      As hardy as the Nemean lion&rsquo;s nerve.
-      <br />
-    </p>
+    <p className="character-name">HORATIO.</p>
+    <p className="word-split">Have after. To what issue will this come?</p>
 
-    <p>
-      [<i>Ghost beckons.</i>]
-    </p>
+    <p className="character-name">MARCELLUS.</p>
+    <p className="word-split">Something is rotten in the state of Denmark.</p>
 
-    <p>Still am I call&rsquo;d. Unhand me, gentlemen.</p>
+    <p className="character-name">HORATIO.</p>
+    <p className="word-split">Heaven will direct it.</p>
 
-    <p>
-      [<i>Breaking free from them.</i>]
-    </p>
+    <p className="character-name">MARCELLUS.</p>
+    <p className="word-split">Nay, let's follow him.</p>
 
-    <p>
-      By heaven, I&rsquo;ll make a ghost of him that lets me.
-      <br />
-      I say, away!&mdash;Go on, I&rsquo;ll follow thee.
-      <br />
-    </p>
-
-    <p>
-      <i>Exeunt Ghost and Hamlet.</i>
-    </p>
-
-    <p>
-      HORATIO.
-      <br />
-      He waxes desperate with imagination.
-      <br />
-    </p>
-
-    <p>
-      MARCELLUS.
-      <br />
-      Let&rsquo;s follow; &rsquo;tis not fit thus to obey him.
-      <br />
-    </p>
-
-    <p>
-      HORATIO.
-      <br />
-      Have after. To what issue will this come?
-      <br />
-    </p>
-
-    <p>
-      MARCELLUS.
-      <br />
-      Something is rotten in the state of Denmark.
-      <br />
-    </p>
-
-    <p>
-      HORATIO.
-      <br />
-      Heaven will direct it.
-      <br />
-    </p>
-
-    <p>
-      MARCELLUS.
-      <br />
-      Nay, let&rsquo;s follow him.
-      <br />
-    </p>
-
-    <p>
-      <i>Exeunt.</i>
-    </p>
+    <p className="stage-direction">Exeunt.</p>
   </div>
 );
 
