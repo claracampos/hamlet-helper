150c9952f330a885facdc161e841a38d75a280a4
claracampos <camposclr@gmail.com>
2020-02-06 17:43:17 -0400

    added act 4, scene 3

---
+++ src/text/act4/Act4Scene3.js
@@ -0,0 +1,148 @@
+import React from "react";
+
+const Act4Scene3 = () => (
+  <div>
+    {" "}
+    <h2>ACT IV</h2>
+    <h3>SCENE III. Another room in the Castle.</h3>
+    <p className="stage-direction"> Enter King, attended.</p>
+    <p className="character-name">KING.</p>
+    <p className="word-split">I have sent to seek him and to find the body.</p>
+    <p className="word-split">How dangerous is it that this man goes loose!</p>
+    <p className="word-split">Yet must not we put the strong law on him:</p>
+    <p className="word-split">He's lov'd of the distracted multitude,</p>
+    <p className="word-split">
+      Who like not in their judgment, but their eyes;
+    </p>
+    <p className="word-split">
+      And where 'tis so, th'offender's scourge is weigh'd,
+    </p>
+    <p className="word-split">
+      But never the offence. To bear all smooth and even,
+    </p>
+    <p className="word-split">This sudden sending him away must seem</p>
+    <p className="word-split">Deliberate pause. Diseases desperate grown</p>
+    <p className="word-split">By desperate appliance are reliev'd,</p>
+    <p className="word-split">Or not at all.</p>
+    <p className="stage-direction"> Enter Rosencrantz.</p>
+    <p className="word-split">How now? What hath befall'n?</p>
+    <p className="character-name">ROSENCRANTZ.</p>
+    <p className="word-split">Where the dead body is bestow'd, my lord,</p>
+    <p className="word-split">We cannot get from him.</p>
+    <p className="character-name">KING.</p>
+    <p className="word-split">But where is he?</p>
+    <p className="character-name">ROSENCRANTZ.</p>
+    <p className="word-split">
+      Without, my lord, guarded, to know your pleasure.
+    </p>
+    <p className="character-name">KING.</p>
+    <p className="word-split">Bring him before us.</p>
+    <p className="character-name">ROSENCRANTZ.</p>
+    <p className="word-split">Ho, Guildenstern! Bring in my lord.</p>
+    <p className="stage-direction"> Enter Hamlet and Guildenstern.</p>
+    <p className="character-name">KING.</p>
+    <p className="word-split">Now, Hamlet, where's Polonius?</p>
+    <p className="character-name">HAMLET.</p>
+    <p className="word-split">At supper.</p>
+    <p className="character-name">KING.</p>
+    <p className="word-split">At supper? Where?</p>
+    <p className="character-name">HAMLET.</p>
+    <p className="word-split">
+      Not where he eats, but where he is eaten. A certain convocation of politic
+      worms are e'en at him. Your worm is your only emperor for diet. We fat all
+      creatures else to fat us, and we fat ourselves for maggots. Your fat king
+      and your lean beggar is but variable service, -- two dishes, but to one
+      table. That's the end.
+    </p>
+    <p className="character-name">KING.</p>
+    <p className="word-split">Alas, alas!</p>
+    <p className="character-name">HAMLET.</p>
+    <p className="word-split">
+      A man may fish with the worm that hath eat of a king, and eat of the fish
+      that hath fed of that worm.
+    </p>
+    <p className="character-name">KING.</p>
+    <p className="word-split">What dost thou mean by this?</p>
+    <p className="character-name">HAMLET.</p>
+    <p className="word-split">
+      Nothing but to show you how a king may go a progress through the guts of a
+      beggar.
+    </p>
+    <p className="character-name">KING.</p>
+    <p className="word-split">Where is Polonius?</p>
+    <p className="character-name">HAMLET.</p>
+    <p className="word-split">
+      In heaven. Send thither to see. If your messenger find him not there, seek
+      him i' th'other place yourself. But indeed, if you find him not within
+      this month, you shall nose him as you go up the stairs into the lobby.
+    </p>
+    <p className="character-name">KING.</p>
+    <p className="word-split">[To some Attendants.] Go seek him there.</p>
+    <p className="character-name">HAMLET.</p>
+    <p className="word-split">He will stay till you come.</p>
+    <p className="stage-direction">[Exeunt Attendants.]</p>
+    <p className="character-name">KING.</p>
+    <p className="word-split">
+      Hamlet, this deed, for thine especial safety, --{" "}
+    </p>
+    <p className="word-split">Which we do tender, as we dearly grieve</p>
+    <p className="word-split">
+      For that which thou hast done, -- must send thee hence
+    </p>
+    <p className="word-split">
+      With fiery quickness. Therefore prepare thyself;
+    </p>
+    <p className="word-split">The bark is ready, and the wind at help,</p>
+    <p className="word-split">Th'associates tend, and everything is bent</p>
+    <p className="word-split">For England.</p>
+    <p className="character-name">HAMLET.</p>
+    <p className="word-split">For England?</p>
+    <p className="character-name">KING.</p>
+    <p className="word-split">Ay, Hamlet.</p>
+    <p className="character-name">HAMLET.</p>
+    <p className="word-split">Good.</p>
+    <p className="character-name">KING.</p>
+    <p className="word-split">So is it, if thou knew'st our purposes.</p>
+    <p className="character-name">HAMLET.</p>
+    <p className="word-split">
+      I see a cherub that sees them. But, come; for England! Farewell, dear
+      mother.
+    </p>
+    <p className="character-name">KING.</p>
+    <p className="word-split">Thy loving father, Hamlet.</p>
+    <p className="character-name">HAMLET.</p>
+    <p className="word-split">
+      My mother. Father and mother is man and wife; man and wife is one flesh;
+      and so, my mother. Come, for England.
+    </p>
+    <p className="stage-direction">[Exit.]</p>
+    <p className="character-name">KING.</p>
+    <p className="word-split">
+      Follow him at foot. Tempt him with speed aboard;
+    </p>
+    <p className="word-split">Delay it not; I'll have him hence tonight.</p>
+    <p className="word-split">Away, for everything is seal'd and done</p>
+    <p className="word-split">
+      That else leans on th'affair. Pray you make haste.
+    </p>
+    <p className="stage-direction">[Exeunt Rosencrantz and Guildenstern.]</p>
+    <p className="word-split">
+      And England, if my love thou hold'st at aught, --
+    </p>
+    <p className="word-split">As my great power thereof may give thee sense,</p>
+    <p className="word-split">Since yet thy cicatrice looks raw and red</p>
+    <p className="word-split">After the Danish sword, and thy free awe</p>
+    <p className="word-split">
+      Pays homage to us, -- thou mayst not coldly set
+    </p>
+    <p className="word-split">Our sovereign process, which imports at full,</p>
+    <p className="word-split">By letters conjuring to that effect,</p>
+    <p className="word-split">The present death of Hamlet. Do it, England;</p>
+    <p className="word-split">For like the hectic in my blood he rages,</p>
+    <p className="word-split">And thou must cure me. Till I know 'tis done,</p>
+    <p className="word-split">Howe'er my haps, my joys were ne'er begun.</p>
+    <p className="stage-direction">[Exit.]</p>
+  </div>
+);
+
+export default Act4Scene3;
