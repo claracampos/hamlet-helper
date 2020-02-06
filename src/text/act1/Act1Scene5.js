c84c057ab17781027b6cf7f46267437246be2b3f
claracampos <camposclr@gmail.com>
2020-02-06 09:42:36 -0400

    added act 1 scene 5 text

---
+++ src/text/act1/Act1Scene5.js
@@ -0,0 +1,749 @@
+import React from "react";
+
+const Act1Scene5 = () => (
+  <div>
+    <h3>SCENE V. A more remote part of the Castle.</h3>
+
+    <p>
+      <i>Enter Ghost and Hamlet.</i>
+    </p>
+
+    <p>
+      HAMLET.
+      <br />
+      Whither wilt thou lead me? Speak, I&rsquo;ll go no further.
+      <br />
+    </p>
+
+    <p>
+      GHOST.
+      <br />
+      Mark me.
+      <br />
+    </p>
+
+    <p>
+      HAMLET.
+      <br />
+      I will.
+      <br />
+    </p>
+
+    <p>
+      GHOST.
+      <br />
+      My hour is almost come,
+      <br />
+      When I to sulph&rsquo;rous and tormenting flames
+      <br />
+      Must render up myself.
+      <br />
+    </p>
+
+    <p>
+      HAMLET.
+      <br />
+      Alas, poor ghost!
+      <br />
+    </p>
+
+    <p>
+      GHOST.
+      <br />
+      Pity me not, but lend thy serious hearing
+      <br />
+      To what I shall unfold.
+      <br />
+    </p>
+
+    <p>
+      HAMLET.
+      <br />
+      Speak, I am bound to hear.
+      <br />
+    </p>
+
+    <p>
+      GHOST.
+      <br />
+      So art thou to revenge, when thou shalt hear.
+      <br />
+    </p>
+
+    <p>
+      HAMLET.
+      <br />
+      What?
+      <br />
+    </p>
+
+    <p>
+      GHOST.
+      <br />
+      I am thy father&rsquo;s spirit,
+      <br />
+      Doom&rsquo;d for a certain term to walk the night,
+      <br />
+      And for the day confin&rsquo;d to fast in fires,
+      <br />
+      Till the foul crimes done in my days of nature
+      <br />
+      Are burnt and purg&rsquo;d away. But that I am forbid
+      <br />
+      To tell the secrets of my prison-house,
+      <br />
+      I could a tale unfold whose lightest word
+      <br />
+      Would harrow up thy soul; freeze thy young blood,
+      <br />
+      Make thy two eyes like stars start from their spheres,
+      <br />
+      Thy knotted and combined locks to part,
+      <br />
+      And each particular hair to stand on end
+      <br />
+      Like quills upon the fretful porcupine.
+      <br />
+      But this eternal blazon must not be
+      <br />
+      To ears of flesh and blood. List, list, O, list!
+      <br />
+      If thou didst ever thy dear father love&mdash;
+      <br />
+    </p>
+
+    <p>
+      HAMLET.
+      <br />
+      O God!
+      <br />
+    </p>
+
+    <p>
+      GHOST.
+      <br />
+      Revenge his foul and most unnatural murder.
+      <br />
+    </p>
+
+    <p>
+      HAMLET.
+      <br />
+      Murder!
+      <br />
+    </p>
+
+    <p>
+      GHOST.
+      <br />
+      Murder most foul, as in the best it is;
+      <br />
+      But this most foul, strange, and unnatural.
+      <br />
+    </p>
+
+    <p>
+      HAMLET.
+      <br />
+      Haste me to know&rsquo;t, that I, with wings as swift
+      <br />
+      As meditation or the thoughts of love
+      <br />
+      May sweep to my revenge.
+      <br />
+    </p>
+
+    <p>
+      GHOST.
+      <br />
+      I find thee apt;
+      <br />
+      And duller shouldst thou be than the fat weed
+      <br />
+      That rots itself in ease on Lethe wharf,
+      <br />
+      Wouldst thou not stir in this. Now, Hamlet, hear.
+      <br />
+      &rsquo;Tis given out that, sleeping in my orchard,
+      <br />
+      A serpent stung me; so the whole ear of Denmark
+      <br />
+      Is by a forged process of my death
+      <br />
+      Rankly abus&rsquo;d; but know, thou noble youth,
+      <br />
+      The serpent that did sting thy father&rsquo;s life
+      <br />
+      Now wears his crown.
+      <br />
+    </p>
+
+    <p>
+      HAMLET.
+      <br />
+      O my prophetic soul!
+      <br />
+      Mine uncle!
+      <br />
+    </p>
+
+    <p>
+      GHOST.
+      <br />
+      Ay, that incestuous, that adulterate beast,
+      <br />
+      With witchcraft of his wit, with traitorous gifts,&mdash;
+      <br />
+      O wicked wit, and gifts, that have the power
+      <br />
+      So to seduce!&mdash;won to his shameful lust
+      <br />
+      The will of my most seeming-virtuous queen.
+      <br />
+      O Hamlet, what a falling off was there,
+      <br />
+      From me, whose love was of that dignity
+      <br />
+      That it went hand in hand even with the vow
+      <br />
+      I made to her in marriage; and to decline
+      <br />
+      Upon a wretch whose natural gifts were poor
+      <br />
+      To those of mine. But virtue, as it never will be mov&rsquo;d,
+      <br />
+      Though lewdness court it in a shape of heaven;
+      <br />
+      So lust, though to a radiant angel link&rsquo;d,
+      <br />
+      Will sate itself in a celestial bed
+      <br />
+      And prey on garbage.
+      <br />
+      But soft! methinks I scent the morning air;
+      <br />
+      Brief let me be. Sleeping within my orchard,
+      <br />
+      My custom always of the afternoon,
+      <br />
+      Upon my secure hour thy uncle stole
+      <br />
+      With juice of cursed hebenon in a vial,
+      <br />
+      And in the porches of my ears did pour
+      <br />
+      The leperous distilment, whose effect
+      <br />
+      Holds such an enmity with blood of man
+      <br />
+      That swift as quicksilver it courses through
+      <br />
+      The natural gates and alleys of the body;
+      <br />
+      And with a sudden vigour it doth posset
+      <br />
+      And curd, like eager droppings into milk,
+      <br />
+      The thin and wholesome blood. So did it mine;
+      <br />
+      And a most instant tetter bark&rsquo;d about,
+      <br />
+      Most lazar-like, with vile and loathsome crust
+      <br />
+      All my smooth body.
+      <br />
+      Thus was I, sleeping, by a brother&rsquo;s hand,
+      <br />
+      Of life, of crown, of queen at once dispatch&rsquo;d:
+      <br />
+      Cut off even in the blossoms of my sin,
+      <br />
+      Unhous&rsquo;led, disappointed, unanel&rsquo;d;
+      <br />
+      No reckoning made, but sent to my account
+      <br />
+      With all my imperfections on my head.
+      <br />
+      O horrible! O horrible! most horrible!
+      <br />
+      If thou hast nature in thee, bear it not;
+      <br />
+      Let not the royal bed of Denmark be
+      <br />
+      A couch for luxury and damned incest.
+      <br />
+      But howsoever thou pursu&rsquo;st this act,
+      <br />
+      Taint not thy mind, nor let thy soul contrive
+      <br />
+      Against thy mother aught; leave her to heaven,
+      <br />
+      And to those thorns that in her bosom lodge,
+      <br />
+      To prick and sting her. Fare thee well at once!
+      <br />
+      The glow-worm shows the matin to be near,
+      <br />
+      And &rsquo;gins to pale his uneffectual fire.
+      <br />
+      Adieu, adieu, adieu. Hamlet, remember me.
+      <br />
+    </p>
+
+    <p>
+      <i>Exit.</i>
+    </p>
+
+    <p>
+      HAMLET.
+      <br />
+      O all you host of heaven! O earth! What else?
+      <br />
+      And shall I couple hell? O, fie! Hold, my heart;
+      <br />
+      And you, my sinews, grow not instant old,
+      <br />
+      But bear me stiffly up. Remember thee?
+      <br />
+      Ay, thou poor ghost, while memory holds a seat
+      <br />
+      In this distracted globe. Remember thee?
+      <br />
+      Yea, from the table of my memory
+      <br />
+      I&rsquo;ll wipe away all trivial fond records,
+      <br />
+      All saws of books, all forms, all pressures past,
+      <br />
+      That youth and observation copied there;
+      <br />
+      And thy commandment all alone shall live
+      <br />
+      Within the book and volume of my brain,
+      <br />
+      Unmix&rsquo;d with baser matter. Yes, by heaven!
+      <br />
+      O most pernicious woman!
+      <br />
+      O villain, villain, smiling damned villain!
+      <br />
+      My tables. Meet it is I set it down,
+      <br />
+      That one may smile, and smile, and be a villain!
+      <br />
+      At least I am sure it may be so in Denmark.
+      <br />
+    </p>
+
+    <p>
+      [<i>Writing.</i>]
+    </p>
+
+    <p>
+      So, uncle, there you are. Now to my word;
+      <br />
+      It is &lsquo;Adieu, adieu, remember me.&rsquo;
+      <br />
+      I have sworn&rsquo;t.
+      <br />
+    </p>
+
+    <p>
+      HORATIO and MARCELLUS.
+      <br />[<i>Within.</i>] My lord, my lord.
+      <br />
+    </p>
+
+    <p>
+      MARCELLUS.
+      <br />[<i>Within.</i>] Lord Hamlet.
+      <br />
+    </p>
+
+    <p>
+      HORATIO.
+      <br />[<i>Within.</i>] Heaven secure him.
+      <br />
+    </p>
+
+    <p>
+      HAMLET.
+      <br />
+      So be it!
+      <br />
+    </p>
+
+    <p>
+      MARCELLUS.
+      <br />[<i>Within.</i>] Illo, ho, ho, my lord!
+      <br />
+    </p>
+
+    <p>
+      HAMLET.
+      <br />
+      Hillo, ho, ho, boy! Come, bird, come.
+      <br />
+    </p>
+
+    <p>
+      <i>Enter Horatio and Marcellus.</i>
+    </p>
+
+    <p>
+      MARCELLUS.
+      <br />
+      How is&rsquo;t, my noble lord?
+      <br />
+    </p>
+
+    <p>
+      HORATIO.
+      <br />
+      What news, my lord?
+      <br />
+    </p>
+
+    <p>
+      HAMLET.
+      <br />
+      O, wonderful!
+      <br />
+    </p>
+
+    <p>
+      HORATIO.
+      <br />
+      Good my lord, tell it.
+      <br />
+    </p>
+
+    <p>
+      HAMLET.
+      <br />
+      No, you&rsquo;ll reveal it.
+      <br />
+    </p>
+
+    <p>
+      HORATIO.
+      <br />
+      Not I, my lord, by heaven.
+      <br />
+    </p>
+
+    <p>
+      MARCELLUS.
+      <br />
+      Nor I, my lord.
+      <br />
+    </p>
+
+    <p>
+      HAMLET.
+      <br />
+      How say you then, would heart of man once think it?&mdash;
+      <br />
+      But you&rsquo;ll be secret?
+      <br />
+    </p>
+
+    <p>
+      HORATIO and MARCELLUS.
+      <br />
+      Ay, by heaven, my lord.
+      <br />
+    </p>
+
+    <p>
+      HAMLET.
+      <br />
+      There&rsquo;s ne&rsquo;er a villain dwelling in all Denmark
+      <br />
+      But he&rsquo;s an arrant knave.
+      <br />
+    </p>
+
+    <p>
+      HORATIO.
+      <br />
+      There needs no ghost, my lord, come from the grave
+      <br />
+      To tell us this.
+      <br />
+    </p>
+
+    <p>
+      HAMLET.
+      <br />
+      Why, right; you are i&rsquo; the right;
+      <br />
+      And so, without more circumstance at all,
+      <br />
+      I hold it fit that we shake hands and part:
+      <br />
+      You, as your business and desires shall point you,&mdash;
+      <br />
+      For every man hath business and desire,
+      <br />
+      Such as it is;&mdash;and for my own poor part,
+      <br />
+      Look you, I&rsquo;ll go pray.
+      <br />
+    </p>
+
+    <p>
+      HORATIO.
+      <br />
+      These are but wild and whirling words, my lord.
+      <br />
+    </p>
+
+    <p>
+      HAMLET.
+      <br />
+      I&rsquo;m sorry they offend you, heartily;
+      <br />
+      Yes faith, heartily.
+      <br />
+    </p>
+
+    <p>
+      HORATIO.
+      <br />
+      There&rsquo;s no offence, my lord.
+      <br />
+    </p>
+
+    <p>
+      HAMLET.
+      <br />
+      Yes, by Saint Patrick, but there is, Horatio,
+      <br />
+      And much offence too. Touching this vision here,
+      <br />
+      It is an honest ghost, that let me tell you.
+      <br />
+      For your desire to know what is between us,
+      <br />
+      O&rsquo;ermaster&rsquo;t as you may. And now, good friends,
+      <br />
+      As you are friends, scholars, and soldiers,
+      <br />
+      Give me one poor request.
+      <br />
+    </p>
+
+    <p>
+      HORATIO.
+      <br />
+      What is&rsquo;t, my lord? We will.
+      <br />
+    </p>
+
+    <p>
+      HAMLET.
+      <br />
+      Never make known what you have seen tonight.
+      <br />
+    </p>
+
+    <p>
+      HORATIO and MARCELLUS.
+      <br />
+      My lord, we will not.
+      <br />
+    </p>
+
+    <p>
+      HAMLET.
+      <br />
+      Nay, but swear&rsquo;t.
+      <br />
+    </p>
+
+    <p>
+      HORATIO.
+      <br />
+      In faith, my lord, not I.
+      <br />
+    </p>
+
+    <p>
+      MARCELLUS.
+      <br />
+      Nor I, my lord, in faith.
+      <br />
+    </p>
+
+    <p>
+      HAMLET.
+      <br />
+      Upon my sword.
+      <br />
+    </p>
+
+    <p>
+      MARCELLUS.
+      <br />
+      We have sworn, my lord, already.
+      <br />
+    </p>
+
+    <p>
+      HAMLET.
+      <br />
+      Indeed, upon my sword, indeed.
+      <br />
+    </p>
+
+    <p>
+      GHOST.
+      <br />[<i>Cries under the stage.</i>] Swear.
+      <br />
+    </p>
+
+    <p>
+      HAMLET.
+      <br />
+      Ha, ha boy, say&rsquo;st thou so? Art thou there, truepenny?
+      <br />
+      Come on, you hear this fellow in the cellarage.
+      <br />
+      Consent to swear.
+      <br />
+    </p>
+
+    <p>
+      HORATIO.
+      <br />
+      Propose the oath, my lord.
+      <br />
+    </p>
+
+    <p>
+      HAMLET.
+      <br />
+      Never to speak of this that you have seen.
+      <br />
+      Swear by my sword.
+      <br />
+    </p>
+
+    <p>
+      GHOST.
+      <br />[<i>Beneath.</i>] Swear.
+      <br />
+    </p>
+
+    <p>
+      HAMLET.
+      <br />
+      <i>Hic et ubique?</i> Then we&rsquo;ll shift our ground.
+      <br />
+      Come hither, gentlemen,
+      <br />
+      And lay your hands again upon my sword.
+      <br />
+      Never to speak of this that you have heard.
+      <br />
+      Swear by my sword.
+      <br />
+    </p>
+
+    <p>
+      GHOST.
+      <br />[<i>Beneath.</i>] Swear.
+      <br />
+    </p>
+
+    <p>
+      HAMLET.
+      <br />
+      Well said, old mole! Canst work i&rsquo; th&rsquo;earth so fast?
+      <br />
+      A worthy pioner! Once more remove, good friends.
+      <br />
+    </p>
+
+    <p>
+      HORATIO.
+      <br />
+      O day and night, but this is wondrous strange.
+      <br />
+    </p>
+
+    <p>
+      HAMLET.
+      <br />
+      And therefore as a stranger give it welcome.
+      <br />
+      There are more things in heaven and earth, Horatio,
+      <br />
+      Than are dreamt of in your philosophy. But come,
+      <br />
+      Here, as before, never, so help you mercy,
+      <br />
+      How strange or odd soe&rsquo;er I bear myself,&mdash;
+      <br />
+      As I perchance hereafter shall think meet
+      <br />
+      To put an antic disposition on&mdash;
+      <br />
+      That you, at such times seeing me, never shall,
+      <br />
+      With arms encumber&rsquo;d thus, or this head-shake,
+      <br />
+      Or by pronouncing of some doubtful phrase,
+      <br />
+      As &lsquo;Well, we know&rsquo;, or &lsquo;We could and if we would&rsquo;,
+      <br />
+      Or &lsquo;If we list to speak&rsquo;; or &lsquo;There be and if they
+      might&rsquo;,
+      <br />
+      Or such ambiguous giving out, to note
+      <br />
+      That you know aught of me:&mdash;this not to do.
+      <br />
+      So grace and mercy at your most need help you,
+      <br />
+      Swear.
+      <br />
+    </p>
+
+    <p>
+      GHOST.
+      <br />[<i>Beneath.</i>] Swear.
+      <br />
+    </p>
+
+    <p>
+      HAMLET.
+      <br />
+      Rest, rest, perturbed spirit. So, gentlemen,
+      <br />
+      With all my love I do commend me to you;
+      <br />
+      And what so poor a man as Hamlet is
+      <br />
+      May do t&rsquo;express his love and friending to you,
+      <br />
+      God willing, shall not lack. Let us go in together,
+      <br />
+      And still your fingers on your lips, I pray.
+      <br />
+      The time is out of joint. O cursed spite,
+      <br />
+      That ever I was born to set it right.
+      <br />
+      Nay, come, let&rsquo;s go together.
+      <br />
+    </p>
+
+    <p>
+      <i>Exeunt.</i>
+    </p>
+  </div>
+);
+
+export default Act1Scene5;
