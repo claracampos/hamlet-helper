32ede06eee452d34c24cc178a999d5030ca203e4
claracampos <camposclr@gmail.com>
2020-02-06 17:42:29 -0400

    added act 2, scene 1

---
+++ src/text/act2/Act2Scene1.js
@@ -0,0 +1,219 @@
+import React from "react";
+
+const Act2Scene1 = () => (
+  <div>
+    <h2>ACT II</h2>
+    <h3>SCENE I. A room in Polonius's house.</h3>
+    <p className="stage-direction">Enter Polonius and Reynaldo.</p>
+    <p className="character-name">POLONIUS.</p>
+    <p className="word-split">Give him this money and these notes, Reynaldo.</p>
+    <p className="character-name">REYNALDO.</p>
+    <p className="word-split">I will, my lord.</p>
+    <p className="character-name">POLONIUS.</p>
+    <p className="word-split">You shall do marvellous wisely, good Reynaldo,</p>
+    <p className="word-split">Before you visit him, to make inquiry</p>
+    <p className="word-split">Of his behaviour.</p>
+    <p className="character-name">REYNALDO.</p>
+    <p className="word-split">My lord, I did intend it.</p>
+    <p className="character-name">POLONIUS.</p>
+    <p className="word-split">
+      Marry, well said; very well said. Look you, sir,
+    </p>
+    <p className="word-split">Enquire me first what Danskers are in Paris;</p>
+    <p className="word-split">
+      And how, and who, what means, and where they keep,
+    </p>
+    <p className="word-split">What company, at what expense; and finding</p>
+    <p className="word-split">By this encompassment and drift of question,</p>
+    <p className="word-split">That they do know my son, come you more nearer</p>
+    <p className="word-split">Than your particular demands will touch it.</p>
+    <p className="word-split">
+      Take you as 'twere some distant knowledge of him,
+    </p>
+    <p className="word-split">As thus, "I know his father and his friends,</p>
+    <p className="word-split">
+      And in part him" -- do you mark this, Reynaldo?
+    </p>
+    <p className="character-name">REYNALDO.</p>
+    <p className="word-split">Ay, very well, my lord.</p>
+    <p className="character-name">POLONIUS.</p>
+    <p className="word-split">
+      "And in part him, but," you may say, "not well;
+    </p>
+    <p className="word-split">But if't be he I mean, he's very wild;</p>
+    <p className="word-split">Addicted so and so;" and there put on him</p>
+    <p className="word-split">What forgeries you please; marry, none so rank</p>
+    <p className="word-split">As may dishonour him; take heed of that;</p>
+    <p className="word-split">But, sir, such wanton, wild, and usual slips</p>
+    <p className="word-split">As are companions noted and most known</p>
+    <p className="word-split">To youth and liberty.</p>
+    <p className="character-name">REYNALDO.</p>
+    <p className="word-split">As gaming, my lord?</p>
+    <p className="character-name">POLONIUS.</p>
+    <p className="word-split">Ay, or drinking, fencing, swearing,</p>
+    <p className="word-split">Quarrelling, drabbing. You may go so far.</p>
+    <p className="character-name">REYNALDO.</p>
+    <p className="word-split">My lord, that would dishonour him.</p>
+    <p className="character-name">POLONIUS.</p>
+    <p className="word-split">Faith no, as you may season it in the charge.</p>
+    <p className="word-split">You must not put another scandal on him,</p>
+    <p className="word-split">That he is open to incontinency;</p>
+    <p className="word-split">
+      That's not my meaning: but breathe his faults so quaintly
+    </p>
+    <p className="word-split">That they may seem the taints of liberty;</p>
+    <p className="word-split">The flash and outbreak of a fiery mind,</p>
+    <p className="word-split">A savageness in unreclaimed blood,</p>
+    <p className="word-split">Of general assault.</p>
+    <p className="character-name">REYNALDO.</p>
+    <p className="word-split">But my good lord -- </p>
+    <p className="character-name">POLONIUS.</p>
+    <p className="word-split">Wherefore should you do this?</p>
+    <p className="character-name">REYNALDO.</p>
+    <p className="word-split">Ay, my lord, I would know that.</p>
+    <p className="character-name">POLONIUS.</p>
+    <p className="word-split">Marry, sir, here's my drift,</p>
+    <p className="word-split">And I believe it is a fetch of warrant.</p>
+    <p className="word-split">You laying these slight sullies on my son,</p>
+    <p className="word-split">
+      As 'twere a thing a little soil'd i' th' working,
+    </p>
+    <p className="word-split">Mark you,</p>
+    <p className="word-split">Your party in converse, him you would sound,</p>
+    <p className="word-split">Having ever seen in the prenominate crimes</p>
+    <p className="word-split">The youth you breathe of guilty, be assur'd</p>
+    <p className="word-split">He closes with you in this consequence;</p>
+    <p className="word-split">
+      "Good sir," or so; or "friend," or "gentleman" --
+    </p>
+    <p className="word-split">According to the phrase or the addition</p>
+    <p className="word-split">Of man and country.</p>
+    <p className="character-name">REYNALDO.</p>
+    <p className="word-split">Very good, my lord.</p>
+    <p className="character-name">POLONIUS.</p>
+    <p className="word-split">And then, sir, does he this, -- </p>
+    <p className="word-split">He does -- What was I about to say?</p>
+    <p className="word-split">
+      By the mass, I was about to say something. Where did I leave?
+    </p>
+    <p className="character-name">REYNALDO.</p>
+    <p className="word-split">At "closes in the consequence."</p>
+    <p className="word-split">At "friend or so,' and "gentleman."</p>
+    <p className="character-name">POLONIUS.</p>
+    <p className="word-split">At "closes in the consequence" ay, marry!</p>
+    <p className="word-split">
+      He closes with you thus: "I know the gentleman,
+    </p>
+    <p className="word-split">I saw him yesterday, or t'other day,</p>
+    <p className="word-split">
+      Or then, or then, with such and such; and, as you say,
+    </p>
+    <p className="word-split">
+      There was he gaming, there o'ertook in's rouse,
+    </p>
+    <p className="word-split">There falling out at tennis": or perchance,</p>
+    <p className="word-split">"I saw him enter such a house of sale" -- </p>
+    <p className="word-split">
+      Videlicet, a brothel, or so forth. See you now;
+    </p>
+    <p className="word-split">
+      Your bait of falsehood takes this carp of truth;
+    </p>
+    <p className="word-split">And thus do we of wisdom and of reach,</p>
+    <p className="word-split">With windlasses, and with assays of bias,</p>
+    <p className="word-split">By indirections find directions out.</p>
+    <p className="word-split">So by my former lecture and advice</p>
+    <p className="word-split">Shall you my son. You have me, have you not?</p>
+    <p className="character-name">REYNALDO.</p>
+    <p className="word-split">My lord, I have.</p>
+    <p className="character-name">POLONIUS.</p>
+    <p className="word-split">God b' wi' you, fare you well.</p>
+    <p className="character-name">REYNALDO.</p>
+    <p className="word-split">Good my lord.</p>
+    <p className="character-name">POLONIUS.</p>
+    <p className="word-split">Observe his inclination in yourself.</p>
+    <p className="character-name">REYNALDO.</p>
+    <p className="word-split">I shall, my lord.</p>
+    <p className="character-name">POLONIUS.</p>
+    <p className="word-split">And let him ply his music.</p>
+    <p className="character-name">REYNALDO.</p>
+    <p className="word-split">Well, my lord.</p>
+    <p className="character-name">POLONIUS.</p>
+    <p className="word-split">Farewell.</p>
+    <p className="stage-direction">[Exit Reynaldo.]</p>
+    <p className="stage-direction">[Enter Ophelia.]</p>
+    <p className="word-split">How now, Ophelia, what's the matter?</p>
+    <p className="character-name">OPHELIA.</p>
+    <p className="word-split">Alas, my lord, I have been so affrighted.</p>
+    <p className="character-name">POLONIUS.</p>
+    <p className="word-split">With what, in the name of God?</p>
+    <p className="character-name">OPHELIA.</p>
+    <p className="word-split">My lord, as I was sewing in my chamber,</p>
+    <p className="word-split">Lord Hamlet, with his doublet all unbrac'd,</p>
+    <p className="word-split">No hat upon his head, his stockings foul'd,</p>
+    <p className="word-split">Ungart'red, and down-gyved to his ankle,</p>
+    <p className="word-split">
+      Pale as his shirt, his knees knocking each other,
+    </p>
+    <p className="word-split">And with a look so piteous in purport</p>
+    <p className="word-split">As if he had been loosed out of hell</p>
+    <p className="word-split">To speak of horrors, he comes before me.</p>
+    <p className="character-name">POLONIUS.</p>
+    <p className="word-split">Mad for thy love?</p>
+    <p className="character-name">OPHELIA.</p>
+    <p className="word-split">
+      My lord, I do not know, but truly I do fear it.
+    </p>
+    <p className="character-name">POLONIUS.</p>
+    <p className="word-split">What said he?</p>
+    <p className="character-name">OPHELIA.</p>
+    <p className="word-split">He took me by the wrist and held me hard;</p>
+    <p className="word-split">Then goes he to the length of all his arm;</p>
+    <p className="word-split">And with his other hand thus o'er his brow,</p>
+    <p className="word-split">He falls to such perusal of my face</p>
+    <p className="word-split">As he would draw it. Long stay'd he so,</p>
+    <p className="word-split">At last, -- a little shaking of mine arm,</p>
+    <p className="word-split">And thrice his head thus waving up and down,</p>
+    <p className="word-split">He rais'd a sigh so piteous and profound</p>
+    <p className="word-split">As it did seem to shatter all his bulk</p>
+    <p className="word-split">And end his being. That done, he lets me go,</p>
+    <p className="word-split">And with his head over his shoulder turn'd</p>
+    <p className="word-split">He seem'd to find his way without his eyes,</p>
+    <p className="word-split">For out o' doors he went without their help,</p>
+    <p className="word-split">And to the last bended their light on me.</p>
+    <p className="character-name">POLONIUS.</p>
+    <p className="word-split">Come, go with me. I will go seek the King.</p>
+    <p className="word-split">This is the very ecstasy of love,</p>
+    <p className="word-split">Whose violent property fordoes itself,</p>
+    <p className="word-split">And leads the will to desperate undertakings,</p>
+    <p className="word-split">As oft as any passion under heaven</p>
+    <p className="word-split">That does afflict our natures. I am sorry, -- </p>
+    <p className="word-split">
+      What, have you given him any hard words of late?
+    </p>
+    <p className="character-name">OPHELIA.</p>
+    <p className="word-split">No, my good lord; but as you did command,</p>
+    <p className="word-split">I did repel his letters and denied</p>
+    <p className="word-split">His access to me.</p>
+    <p className="character-name">POLONIUS.</p>
+    <p className="word-split">That hath made him mad.</p>
+    <p className="word-split">I am sorry that with better heed and judgment</p>
+    <p className="word-split">
+      I had not quoted him. I fear'd he did but trifle,
+    </p>
+    <p className="word-split">
+      And meant to wreck thee. But beshrew my jealousy!
+    </p>
+    <p className="word-split">It seems it is as proper to our age</p>
+    <p className="word-split">To cast beyond ourselves in our opinions</p>
+    <p className="word-split">As it is common for the younger sort</p>
+    <p className="word-split">To lack discretion. Come, go we to the King.</p>
+    <p className="word-split">
+      This must be known, which, being kept close, might move
+    </p>
+    <p className="word-split">More grief to hide than hate to utter love.</p>
+    <p className="stage-direction">[Exeunt.]</p>
+  </div>
+);
+
+export default Act2Scene1;
