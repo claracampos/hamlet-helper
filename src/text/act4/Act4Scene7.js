1d924b772191b5c3d1dbcde4e49f75ff3e250741
claracampos <camposclr@gmail.com>
2020-02-06 17:43:36 -0400

    added act 4, scene 7

---
+++ src/text/act4/Act4Scene7.js
@@ -0,0 +1,354 @@
+import React from "react";
+
+const Act4Scene7 = () => (
+  <div>
+    <h2>ACT IV</h2>
+    <h3>SCENE VII. Another room in the Castle.</h3>
+    <p className="stage-direction"> Enter King and Laertes.</p>
+    <p className="character-name">KING.</p>
+    <p className="word-split">Now must your conscience my acquittance seal,</p>
+    <p className="word-split">And you must put me in your heart for friend,</p>
+    <p className="word-split">Sith you have heard, and with a knowing ear,</p>
+    <p className="word-split">That he which hath your noble father slain</p>
+    <p className="word-split">Pursu'd my life.</p>
+    <p className="character-name">LAERTES.</p>
+    <p className="word-split">It well appears. But tell me</p>
+    <p className="word-split">Why you proceeded not against these feats,</p>
+    <p className="word-split">So crimeful and so capital in nature,</p>
+    <p className="word-split">As by your safety, wisdom, all things else,</p>
+    <p className="word-split">You mainly were stirr'd up.</p>
+    <p className="character-name">KING.</p>
+    <p className="word-split">O, for two special reasons,</p>
+    <p className="word-split">
+      Which may to you, perhaps, seem much unsinew'd,
+    </p>
+    <p className="word-split">
+      But yet to me they are strong. The Queen his mother
+    </p>
+    <p className="word-split">Lives almost by his looks; and for myself, -- </p>
+    <p className="word-split">
+      My virtue or my plague, be it either which, --{" "}
+    </p>
+    <p className="word-split">She's so conjunctive to my life and soul,</p>
+    <p className="word-split">That, as the star moves not but in his sphere,</p>
+    <p className="word-split">I could not but by her. The other motive,</p>
+    <p className="word-split">Why to a public count I might not go,</p>
+    <p className="word-split">Is the great love the general gender bear him,</p>
+    <p className="word-split">
+      Who, dipping all his faults in their affection,
+    </p>
+    <p className="word-split">
+      Would like the spring that turneth wood to stone,
+    </p>
+    <p className="word-split">
+      Convert his gyves to graces; so that my arrows,
+    </p>
+    <p className="word-split">Too slightly timber'd for so loud a wind,</p>
+    <p className="word-split">Would have reverted to my bow again,</p>
+    <p className="word-split">And not where I had aim'd them.</p>
+    <p className="character-name">LAERTES.</p>
+    <p className="word-split">And so have I a noble father lost,</p>
+    <p className="word-split">A sister driven into desperate terms,</p>
+    <p className="word-split">Whose worth, if praises may go back again,</p>
+    <p className="word-split">Stood challenger on mount of all the age</p>
+    <p className="word-split">For her perfections. But my revenge will come.</p>
+    <p className="character-name">KING.</p>
+    <p className="word-split">
+      Break not your sleeps for that. You must not think
+    </p>
+    <p className="word-split">That we are made of stuff so flat and dull</p>
+    <p className="word-split">
+      That we can let our beard be shook with danger,
+    </p>
+    <p className="word-split">
+      And think it pastime. You shortly shall hear more.
+    </p>
+    <p className="word-split">I lov'd your father, and we love ourself,</p>
+    <p className="word-split">
+      And that, I hope, will teach you to imagine --{" "}
+    </p>
+    <p className="stage-direction"> Enter a Messenger.</p>
+    <p className="word-split">How now? What news?</p>
+    <p className="character-name">MESSENGER.</p>
+    <p className="word-split">Letters, my lord, from Hamlet.</p>
+    <p className="word-split">This to your Majesty; this to the Queen.</p>
+    <p className="character-name">KING.</p>
+    <p className="word-split">From Hamlet! Who brought them?</p>
+    <p className="character-name">MESSENGER.</p>
+    <p className="word-split">Sailors, my lord, they say; I saw them not.</p>
+    <p className="word-split">
+      They were given me by Claudio. He receiv'd them
+    </p>
+    <p className="word-split">Of him that brought them.</p>
+    <p className="character-name">KING.</p>
+    <p className="word-split">Laertes, you shall hear them.</p>
+    <p className="word-split">Leave us.</p>
+    <p className="stage-direction">[Exit Messenger.]</p>
+    <p className="word-split">
+      [Reads.] "High and mighty, you shall know I am set naked on your kingdom.
+      Tomorrow shall I beg leave to see your kingly eyes. When I shall, first
+      asking your pardon thereunto, recount the occasions of my sudden and more
+      strange return.
+    </p>
+    <p className="word-split">HAMLET."</p>
+    <p className="word-split">
+      What should this mean? Are all the rest come back?
+    </p>
+    <p className="word-split">Or is it some abuse, and no such thing?</p>
+    <p className="character-name">LAERTES.</p>
+    <p className="word-split">Know you the hand?</p>
+    <p className="character-name">KING.</p>
+    <p className="word-split">'Tis Hamlet's character. 'Naked!'</p>
+    <p className="word-split">And in a postscript here he says "alone."</p>
+    <p className="word-split">Can you advise me?</p>
+    <p className="character-name">LAERTES.</p>
+    <p className="word-split">I am lost in it, my lord. But let him come,</p>
+    <p className="word-split">It warms the very sickness in my heart</p>
+    <p className="word-split">That I shall live and tell him to his teeth,</p>
+    <p className="word-split">"Thus diest thou."</p>
+    <p className="character-name">KING.</p>
+    <p className="word-split">If it be so, Laertes, -- </p>
+    <p className="word-split">As how should it be so? How otherwise? -- </p>
+    <p className="word-split">Will you be rul'd by me?</p>
+    <p className="character-name">LAERTES.</p>
+    <p className="word-split">Ay, my lord;</p>
+    <p className="word-split">So you will not o'errule me to a peace.</p>
+    <p className="character-name">KING.</p>
+    <p className="word-split">To thine own peace. If he be now return'd,</p>
+    <p className="word-split">As checking at his voyage, and that he means</p>
+    <p className="word-split">No more to undertake it, I will work him</p>
+    <p className="word-split">To exploit, now ripe in my device,</p>
+    <p className="word-split">Under the which he shall not choose but fall;</p>
+    <p className="word-split">And for his death no wind shall breathe,</p>
+    <p className="word-split">
+      But even his mother shall uncharge the practice
+    </p>
+    <p className="word-split">And call it accident.</p>
+    <p className="character-name">LAERTES.</p>
+    <p className="word-split">My lord, I will be rul'd;</p>
+    <p className="word-split">The rather if you could devise it so</p>
+    <p className="word-split">That I might be the organ.</p>
+    <p className="character-name">KING.</p>
+    <p className="word-split">It falls right.</p>
+    <p className="word-split">
+      You have been talk'd of since your travel much,
+    </p>
+    <p className="word-split">And that in Hamlet's hearing, for a quality</p>
+    <p className="word-split">Wherein they say you shine. Your sum of parts</p>
+    <p className="word-split">Did not together pluck such envy from him</p>
+    <p className="word-split">As did that one, and that, in my regard,</p>
+    <p className="word-split">Of the unworthiest siege.</p>
+    <p className="character-name">LAERTES.</p>
+    <p className="word-split">What part is that, my lord?</p>
+    <p className="character-name">KING.</p>
+    <p className="word-split">A very riband in the cap of youth,</p>
+    <p className="word-split">Yet needful too, for youth no less becomes</p>
+    <p className="word-split">The light and careless livery that it wears</p>
+    <p className="word-split">Than settled age his sables and his weeds,</p>
+    <p className="word-split">
+      Importing health and graveness. Two months since
+    </p>
+    <p className="word-split">Here was a gentleman of Normandy, -- </p>
+    <p className="word-split">
+      I've seen myself, and serv'd against, the French,
+    </p>
+    <p className="word-split">
+      And they can well on horseback, but this gallant
+    </p>
+    <p className="word-split">Had witchcraft in't. He grew unto his seat,</p>
+    <p className="word-split">And to such wondrous doing brought his horse,</p>
+    <p className="word-split">As had he been incorps'd and demi-natur'd</p>
+    <p className="word-split">
+      With the brave beast. So far he topp'd my thought
+    </p>
+    <p className="word-split">That I in forgery of shapes and tricks,</p>
+    <p className="word-split">Come short of what he did.</p>
+    <p className="character-name">LAERTES.</p>
+    <p className="word-split">A Norman was't?</p>
+    <p className="character-name">KING.</p>
+    <p className="word-split">A Norman.</p>
+    <p className="character-name">LAERTES.</p>
+    <p className="word-split">Upon my life, Lamond.</p>
+    <p className="character-name">KING.</p>
+    <p className="word-split">The very same.</p>
+    <p className="character-name">LAERTES.</p>
+    <p className="word-split">I know him well. He is the brooch indeed</p>
+    <p className="word-split">And gem of all the nation.</p>
+    <p className="character-name">KING.</p>
+    <p className="word-split">He made confession of you,</p>
+    <p className="word-split">And gave you such a masterly report</p>
+    <p className="word-split">For art and exercise in your defence,</p>
+    <p className="word-split">And for your rapier most especially,</p>
+    <p className="word-split">That he cried out 'twould be a sight indeed</p>
+    <p className="word-split">
+      If one could match you. The scrimers of their nation
+    </p>
+    <p className="word-split">He swore had neither motion, guard, nor eye,</p>
+    <p className="word-split">If you oppos'd them. Sir, this report of his</p>
+    <p className="word-split">Did Hamlet so envenom with his envy</p>
+    <p className="word-split">That he could nothing do but wish and beg</p>
+    <p className="word-split">Your sudden coming o'er to play with him.</p>
+    <p className="word-split">Now, out of this, -- </p>
+    <p className="character-name">LAERTES.</p>
+    <p className="word-split">What out of this, my lord?</p>
+    <p className="character-name">KING.</p>
+    <p className="word-split">Laertes, was your father dear to you?</p>
+    <p className="word-split">Or are you like the painting of a sorrow,</p>
+    <p className="word-split">A face without a heart?</p>
+    <p className="character-name">LAERTES.</p>
+    <p className="word-split">Why ask you this?</p>
+    <p className="character-name">KING.</p>
+    <p className="word-split">Not that I think you did not love your father,</p>
+    <p className="word-split">But that I know love is begun by time,</p>
+    <p className="word-split">And that I see, in passages of proof,</p>
+    <p className="word-split">Time qualifies the spark and fire of it.</p>
+    <p className="word-split">There lives within the very flame of love</p>
+    <p className="word-split">A kind of wick or snuff that will abate it;</p>
+    <p className="word-split">And nothing is at a like goodness still,</p>
+    <p className="word-split">For goodness, growing to a pleurisy,</p>
+    <p className="word-split">Dies in his own too much. That we would do,</p>
+    <p className="word-split">
+      We should do when we would; for this "would" changes,
+    </p>
+    <p className="word-split">And hath abatements and delays as many</p>
+    <p className="word-split">
+      As there are tongues, are hands, are accidents;
+    </p>
+    <p className="word-split">
+      And then this "should" is like a spendthrift sigh
+    </p>
+    <p className="word-split">
+      That hurts by easing. But to the quick o' th'ulcer:
+    </p>
+    <p className="word-split">Hamlet comes back: what would you undertake</p>
+    <p className="word-split">To show yourself your father's son in deed,</p>
+    <p className="word-split">More than in words?</p>
+    <p className="character-name">LAERTES.</p>
+    <p className="word-split">To cut his throat i' th' church.</p>
+    <p className="character-name">KING.</p>
+    <p className="word-split">No place, indeed, should murder sanctuarize;</p>
+    <p className="word-split">
+      Revenge should have no bounds. But good Laertes,
+    </p>
+    <p className="word-split">
+      Will you do this, keep close within your chamber.
+    </p>
+    <p className="word-split">Hamlet return'd shall know you are come home:</p>
+    <p className="word-split">
+      We'll put on those shall praise your excellence,
+    </p>
+    <p className="word-split">And set a double varnish on the fame</p>
+    <p className="word-split">
+      The Frenchman gave you, bring you in fine together
+    </p>
+    <p className="word-split">And wager on your heads. He, being remiss,</p>
+    <p className="word-split">Most generous, and free from all contriving,</p>
+    <p className="word-split">Will not peruse the foils; so that with ease,</p>
+    <p className="word-split">Or with a little shuffling, you may choose</p>
+    <p className="word-split">A sword unbated, and in a pass of practice,</p>
+    <p className="word-split">Requite him for your father.</p>
+    <p className="character-name">LAERTES.</p>
+    <p className="word-split">I will do't.</p>
+    <p className="word-split">And for that purpose I'll anoint my sword.</p>
+    <p className="word-split">I bought an unction of a mountebank</p>
+    <p className="word-split">So mortal that, but dip a knife in it,</p>
+    <p className="word-split">Where it draws blood no cataplasm so rare,</p>
+    <p className="word-split">Collected from all simples that have virtue</p>
+    <p className="word-split">Under the moon, can save the thing from death</p>
+    <p className="word-split">
+      This is but scratch'd withal. I'll touch my point
+    </p>
+    <p className="word-split">
+      With this contagion, that if I gall him slightly,
+    </p>
+    <p className="word-split">It may be death.</p>
+    <p className="character-name">KING.</p>
+    <p className="word-split">Let's further think of this,</p>
+    <p className="word-split">Weigh what convenience both of time and means</p>
+    <p className="word-split">May fit us to our shape. If this should fail,</p>
+    <p className="word-split">
+      And that our drift look through our bad performance.
+    </p>
+    <p className="word-split">
+      'Twere better not assay'd. Therefore this project
+    </p>
+    <p className="word-split">Should have a back or second, that might hold</p>
+    <p className="word-split">If this did blast in proof. Soft, let me see.</p>
+    <p className="word-split">
+      We'll make a solemn wager on your cunnings, --{" "}
+    </p>
+    <p className="word-split">
+      I ha't! When in your motion you are hot and dry,
+    </p>
+    <p className="word-split">As make your bouts more violent to that end,</p>
+    <p className="word-split">
+      And that he calls for drink, I'll have prepar'd him
+    </p>
+    <p className="word-split">A chalice for the nonce; whereon but sipping,</p>
+    <p className="word-split">If he by chance escape your venom'd stuck,</p>
+    <p className="word-split">Our purpose may hold there.</p>
+    <p className="stage-direction"> Enter Queen.</p>
+    <p className="word-split">How now, sweet Queen?</p>
+    <p className="character-name">QUEEN.</p>
+    <p className="word-split">One woe doth tread upon another's heel,</p>
+    <p className="word-split">
+      So fast they follow. Your sister's drown'd, Laertes.
+    </p>
+    <p className="character-name">LAERTES.</p>
+    <p className="word-split">Drown'd! O, where?</p>
+    <p className="character-name">QUEEN.</p>
+    <p className="word-split">There is a willow grows aslant a brook,</p>
+    <p className="word-split">
+      That shows his hoary leaves in the glassy stream.
+    </p>
+    <p className="word-split">There with fantastic garlands did she make</p>
+    <p className="word-split">
+      Of crow-flowers, nettles, daisies, and long purples,
+    </p>
+    <p className="word-split">That liberal shepherds give a grosser name,</p>
+    <p className="word-split">
+      But our cold maids do dead men's fingers call them.
+    </p>
+    <p className="word-split">There on the pendant boughs her coronet weeds</p>
+    <p className="word-split">Clamb'ring to hang, an envious sliver broke,</p>
+    <p className="word-split">When down her weedy trophies and herself</p>
+    <p className="word-split">
+      Fell in the weeping brook. Her clothes spread wide,
+    </p>
+    <p className="word-split">And mermaid-like, awhile they bore her up,</p>
+    <p className="word-split">Which time she chaunted snatches of old tunes,</p>
+    <p className="word-split">As one incapable of her own distress,</p>
+    <p className="word-split">Or like a creature native and indued</p>
+    <p className="word-split">Unto that element. But long it could not be</p>
+    <p className="word-split">
+      Till that her garments, heavy with their drink,
+    </p>
+    <p className="word-split">Pull'd the poor wretch from her melodious lay</p>
+    <p className="word-split">To muddy death.</p>
+    <p className="character-name">LAERTES.</p>
+    <p className="word-split">Alas, then she is drown'd?</p>
+    <p className="character-name">QUEEN.</p>
+    <p className="word-split">Drown'd, drown'd.</p>
+    <p className="character-name">LAERTES.</p>
+    <p className="word-split">Too much of water hast thou, poor Ophelia,</p>
+    <p className="word-split">And therefore I forbid my tears. But yet</p>
+    <p className="word-split">It is our trick; nature her custom holds,</p>
+    <p className="word-split">
+      Let shame say what it will. When these are gone,
+    </p>
+    <p className="word-split">The woman will be out. Adieu, my lord,</p>
+    <p className="word-split">
+      I have a speech of fire, that fain would blaze,
+    </p>
+    <p className="word-split">But that this folly douts it.</p>
+    <p className="stage-direction">[Exit.]</p>
+    <p className="character-name">KING.</p>
+    <p className="word-split">Let's follow, Gertrude;</p>
+    <p className="word-split">How much I had to do to calm his rage!</p>
+    <p className="word-split">Now fear I this will give it start again;</p>
+    <p className="word-split">Therefore let's follow.</p>
+    <p className="stage-direction">[Exeunt.]</p>
+  </div>
+);
+
+export default Act4Scene7;
