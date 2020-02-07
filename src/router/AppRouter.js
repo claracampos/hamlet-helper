df29fa01945f4385cba33aaa4c18926d84a3bf14
claracampos <camposclr@gmail.com>
2020-02-07 11:07:44 -0400

    added dramatis personae page to routes

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -2,12 +2,16 @@ import React from "react";
 import { Route, Switch, BrowserRouter } from "react-router-dom";
 import Homepage from "../pages/Homepage";
 import PlayRoutes from "./PlayRoutes";
+import DramatisPersonaePage from "../pages/DramatisPersonaePage";
 
 const AppRouter = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/">
         <Homepage />
+      </Route>
+      <Route exact path="/dramatis-personae">
+        <DramatisPersonaePage />
       </Route>
       <PlayRoutes />
     </Switch>
