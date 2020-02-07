3519801b74230501ccaa3c3457758fb5a7337bf8
claracampos <camposclr@gmail.com>
2020-02-07 11:07:34 -0400

    created Dramatis Personae page

---
+++ src/pages/DramatisPersonaePage.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+const DramatisPersonaePage = () => (
+  <div>
+    <h1>Dramatis Personae</h1>
+    <ul>
+      <li>HAMLET, Prince of Denmark.</li>
+      <li>CLAUDIUS, King of Denmark, Hamlet’s uncle.</li>
+      <li>The GHOST of the late king, Hamlet’s father.</li>
+      <li>GERTRUDE, the Queen, Hamlet’s mother, now wife of Claudius.</li>
+      <li>POLONIUS, Lord Chamberlain.</li>
+      <li>LAERTES, Son to Polonius.</li>
+      <li>OPHELIA, Daughter to Polonius.</li>
+      <li>HORATIO, Friend to Hamlet.</li>
+      <li>FORTINBRAS, Prince of Norway.</li>
+      <li>VOLTEMAND, Courtier.</li>
+      <li>CORNELIUS, Courtier.</li>
+      <li>ROSENCRANTZ, Courtier.</li>
+      <li>GUILDENSTERN, Courtier.</li>
+      <li>MARCELLUS, Officer.</li>
+      <li>BARNARDO, Officer.</li>
+      <li>FRANCISCO, a Soldier</li>
+      <li>OSRIC, Courtier.</li>
+      <li>REYNALDO, Servant to Polonius.</li>
+      <li>Players.</li>
+      <li>A Gentleman, Courtier.</li>
+      <li>A Priest.</li>
+      <li>Two Clowns, Grave-diggers.</li>
+      <li>A Captain.</li>
+      <li>English Ambassadors.</li>
+      <li>
+        Lords, Ladies, Officers, Soldiers, Sailors, Messengers, and Attendants.
+      </li>
+    </ul>
+  </div>
+);
+
+export default DramatisPersonaePage;
