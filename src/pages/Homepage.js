3c8c02e1f41a16a669ebab6b055147961cda1486
claracampos <camposclr@gmail.com>
2020-02-07 13:58:57 -0400

    styled page and added state to display definition

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,41 +1,63 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import will from "../media/will.jpg";
+import skull from "../media/skull.jpg";
 
-const Homepage = () => (
-  <div>
-    <h1>Hamlet Helper</h1>
-    <h2>Description here</h2>
-    <p>Dramatis Personae</p>
-    <p>ACT I</p>
-    <Link to="/act1/scene1">
-      Scene I. Elsinore. A platform before the Castle.
-    </Link>
-    <Link to="/act1/scene2">
-      Scene II. Elsinore. A room of state in the Castle
-    </Link>
-    <Link to="/act1/scene3">Scene III. A room in Polonius’s house.</Link>
-    <Link to="/act1/scene4">Scene IV. The platform.</Link>
-    <Link to="/act1/scene5">Scene V. A more remote part of the Castle.</Link>
-    <p>ACT II</p>
-    <Link to="/act2/scene1">Scene I. A room in Polonius’s house.</Link>
-    <Link to="/act2/scene2">Scene II. A room in the Castle.</Link>
-    <p>ACT III</p>
-    <Link to="/act3/scene1">Scene I. A room in the Castle.</Link>
-    <Link to="/act3/scene2">Scene II. A hall in the Castle.</Link>
-    <Link to="/act3/scene3">Scene III. A room in the Castle.</Link>
-    <Link to="/act3/scene4">Scene IV. Another room in the Castle.</Link>
-    <p>ACT IV</p>
-    <Link to="/act4/scene1">Scene I. A room in the Castle.</Link>
-    <Link to="/act4/scene2">Scene II. Another room in the Castle.</Link>
-    <Link to="/act4/scene3">Scene III. Another room in the Castle.</Link>
-    <Link to="/act4/scene4">Scene IV. A plain in Denmark.</Link>
-    <Link to="/act4/scene5">Scene V. Elsinore. A room in the Castle.</Link>
-    <Link to="/act4/scene6">Scene VI. Another room in the Castle.</Link>
-    <Link to="/act4/scene7">Scene VII. Another room in the Castle.</Link>
-    <p>ACT V</p>
-    <Link to="/act5/scene1">Scene I. A churchyard.</Link>
-    <Link to="/act5/scene2">Scene II. A hall in the Castle.</Link>
-  </div>
-);
+const Homepage = () => {
+  const [showDefiniton, setShowDefinition] = useState(false);
+
+  return (
+    <div className="d-flex flex-column text-center justify-content-center bg-yellow">
+      <div className="text-center">
+        <img
+          class="main-image"
+          src={will}
+          alt='William Shakespeare with sunglasses on and the words "Hamlet Helper" overlaid.'
+        />
+      </div>
+      <div className="mt-3 px-1">
+        <h2>
+          Shakespeare's <i>Hamlet</i> with an embedded dictionary.
+        </h2>
+      </div>
+      <div className="px-3 d-flex flex-column justify-content-center">
+        <p className="align-self-center text-justify">
+          No idea what good old Will was saying? No problem!
+          <b> Click the word and the definition will appear.</b>
+        </p>
+        <p className="align-self-center text-justify my-2">
+          (Now you can actually read <i>Hamlet</i> instead of watching the movie
+          and pretending that you did.)
+        </p>
+      </div>
+      <div className="text-center my-4 d-flex flex-row flex-wrap justify-content-center">
+        <img
+          class="skull-image"
+          src={skull}
+          alt="Illustration of a human skull with an asterisk next to it"
+          onClick={() => setShowDefinition(!showDefiniton)}
+        />
+        {showDefiniton && (
+          <div className="skull-definition text-justify text-white p-3 m-3 shadow">
+            <p className="h4">skull</p>
+            <p>
+              <i>noun:</i> the skeleton of the head of a vertebrate forming a
+              bony or cartilaginous case that encloses and protects the brain
+              and chief sense organs and supports the jaws.
+              <br />
+              <i>verb:</i> to hit on the head.
+            </p>
+            <a
+              href="https://www.merriam-webster.com/dictionary/skull"
+              target="blank"
+              className="btn btn-primary py-0 btn-sm"
+            >
+              View More
+            </a>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default Homepage;
