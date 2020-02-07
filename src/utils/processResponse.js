23750ebe155bd88591474caab8b624fdedf1154e
claracampos <camposclr@gmail.com>
2020-02-07 19:31:13 -0400

    altered function return and renamed it processReponse

--- src/utils/processResponse.js
+++ src/utils/processResponse.js
@@ -1,6 +1,9 @@
-const getDefinitionText = response => {
+const processResponse = response => {
   if (response.data[0]) {
     const headword = response.data[0].hwi.hw.toLowerCase();
+    if (headword === "c & w") {
+      throw Error;
+    }
 
     const matchingWords = response.data.filter(
       item => item.hwi.hw.toLowerCase() === headword
@@ -9,7 +12,10 @@ const getDefinitionText = response => {
     const offensiveContent = matchingWords.find(item => item.meta.offensive);
 
     if (offensiveContent) {
-      return `${headword} - the list of definitions for this word includes sensitive or offensive content. Click "View More" to read definitions.`;
+      return {
+        word: headword,
+        definition: `the list of definitions for this word includes sensitive or offensive content. Click "View More" to read definitions.`
+      };
     }
 
     const matchingDefinitions = matchingWords.map(item => {
@@ -28,9 +34,9 @@ const getDefinitionText = response => {
     });
     console.log(matchingDefinitions);
 
-    const definitionText = `${headword} - ${matchingDefinitions}`;
-    return definitionText;
+    const APIdefinitions = { word: headword, definition: matchingDefinitions };
+    return APIdefinitions;
   }
 };
 
-export default getDefinitionText;
+export default processResponse;
