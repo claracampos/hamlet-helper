38f71a5dfea9db844be42496dca735245df74524
claracampos <camposclr@gmail.com>
2020-02-07 10:59:41 -0400

    created listOfContents

---
+++ src/text/listOfContents.js
@@ -0,0 +1,51 @@
+const listOfContents = [
+  {
+    path: "act1",
+    title: "Act I",
+    scenes: [
+      "Scene I. Elsinore. A platform before the Castle.",
+      "Scene II. Elsinore. A room of state in the Castle",
+      "Scene III. A room in Polonius’s house.",
+      "Scene IV. The platform.",
+      "Scene V. A more remote part of the Castle."
+    ]
+  },
+  {
+    path: "act2",
+    title: "Act II",
+    scenes: [
+      "Scene I. A room in Polonius’s house.",
+      "Scene II. A room in the Castle."
+    ]
+  },
+  {
+    path: "act3",
+    title: "Act III",
+    scenes: [
+      "Scene I. A room in the Castle.",
+      "Scene II. A hall in the Castle.",
+      "Scene III. A room in the Castle.",
+      "Scene IV. Another room in the Castle."
+    ]
+  },
+  {
+    path: "act4",
+    title: "Act IV",
+    scenes: [
+      "Scene I. A room in the Castle.",
+      "Scene II. Another room in the Castle.",
+      "Scene III. Another room in the Castle.",
+      "Scene IV. A plain in Denmark.",
+      "Scene V. Elsinore. A room in the Castle.",
+      "Scene VI. Another room in the Castle.",
+      "Scene VII. Another room in the Castle."
+    ]
+  },
+  {
+    path: "act5",
+    title: "Act V",
+    scenes: ["Scene I. A churchyard.", "Scene II. A hall in the Castle."]
+  }
+];
+
+export default listOfContents;
