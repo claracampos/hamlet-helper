33429c730f3a7eaeabfbc7e9329625f3d8dc9ec7
claracampos <camposclr@gmail.com>
2020-02-10 15:26:23 -0400

    added content and style

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -1,8 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NotFoundPage = () => (
-  <div>
-    <h1>404</h1>
+  <div className="p-5 d-flex flex-column justify-content-center align-items-center">
+    <h1 className="display-2">404</h1>
+    <p className="lead">The page you requested could not be found.</p>
+    <Link to="/" className="btn btn-light mt-4">
+      Go home
+    </Link>
   </div>
 );
 
