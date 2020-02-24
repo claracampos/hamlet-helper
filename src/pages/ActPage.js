7de434fae1eb2f9e3799cf9dc06f79847585c7ba
claracampos <camposclr@gmail.com>
2020-02-24 00:05:17 -0400

    added url params and a redirect to handle bad urls

--- src/pages/ActPage.js
+++ src/pages/ActPage.js
@@ -1,17 +1,21 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useParams, Redirect } from "react-router-dom";
 import listOfContents from "../text/listOfContents";
 
-const ActPage = props => {
-  const act = listOfContents.find(act => act.path === props.path);
-  return (
+const ActPage = () => {
+  const { act: actNumber } = useParams();
+  const act = listOfContents[parseInt(actNumber) - 1];
+
+  return !act ? (
+    <Redirect to="/404" />
+  ) : (
     <div className="bg-yellow p-5 d-flex flex-column align-items-center">
       <h1 className="mb-4 font-italic">{act.title}</h1>
       <div className="d-flex flex-column ">
         {act.scenes.map((item, index) => {
           return (
             <Link
-              to={`/${act.path}/scene${index + 1}`}
+              to={`/act/${actNumber}/scene/${index + 1}`}
               className="btn btn-outline-dark text-black font-weight-bold mb-2"
               key={`scene${index + 1}`}
             >
