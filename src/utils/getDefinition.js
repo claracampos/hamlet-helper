8417f2822961ea8673bd74ae17ddd29dbf30c00f
claracampos <camposclr@gmail.com>
2020-02-24 01:11:49 -0400

    added "error: true" value to state in case of errors

--- src/utils/getDefinition.js
+++ src/utils/getDefinition.js
@@ -1,25 +1,25 @@
 import axios from "axios";
 import processResponse from "./processResponse";
 
-const getDefinition = async (e, setState) => {
+const getDefinition = async (e, setDefinition) => {
   if (e.target.nodeName === "SPAN") {
     const word = e.target.innerText
       .toLowerCase()
       .replace("?", "")
       .replace("'s", "")
       .replace("'d", "ed");
+
     try {
       const response = await axios.get(`https://www.dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=${process.env.REACT_APP_KEY}
       `);
+
       const APIDefinitions = processResponse(response);
-      setState({
-        word: APIDefinitions.word,
-        definition: APIDefinitions.definition
-      });
-    } catch (error) {
-      setState({
-        word: "error",
-        definition: ""
+      setDefinition(APIDefinitions);
+    } catch {
+      setDefinition({
+        word: "",
+        definition: "",
+        error: true
       });
     }
   }
